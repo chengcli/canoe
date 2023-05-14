f1c1519cd9dbff243aa160e4ff8026d9f298735f
chengcli <chengcli@umich.edu>
2023-05-15 00:36:26 +0200

    add debugger

---
+++ src/debugger/debugger_impl.hpp
@@ -0,0 +1,35 @@
+// C/C++ headers
+#include <iostream>
+
+// debugger headers
+#include "debugger.hpp"
+
+template<typename T>
+Debugger* Debugger::Message(std::string str, T const& a) {
+  msg << "- " << str << " = " << a << std::endl;
+  return this;
+}
+
+template<typename T>
+Debugger* Debugger::Message(std::string str, T* a, int n) {
+  msg << "- " << str << " = ";
+  for (int i = 0; i < n; ++i)
+    msg << a[i] << " ";
+  msg << std::endl;
+  return this;
+}
+
+template<typename T>
+Debugger* Debugger::Message(std::string str, std::vector<T> &a) {
+  msg << "- " << str << " = ";
+  for (size_t i = 0; i < a.size(); ++i)
+    msg << a[i] << " ";
+  msg << std::endl;
+  return this;
+}
+
+template<typename T>
+void Debugger::Print(std::string name, T const& value){
+  if (Globals::my_rank == 0) 
+    std::cout << name << " = " << value << std::endl;
+}
