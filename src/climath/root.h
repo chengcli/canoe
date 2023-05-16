72dbdbda238e69b4a976ec5d49a439fd188a2e80
chengcli <chengcli@umich.edu>
2023-05-16 08:44:32 +0100

    sprintf -> snprintf

--- src/climath/root.h
+++ src/climath/root.h
@@ -1,7 +1,15 @@
 #ifndef ROOT_H_
 #define ROOT_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
 typedef double (*RootFunction_t)(double, void*);
 int root(double x1, double x2, double xacc, double *x_root, RootFunction_t func, void *aux);
 
 #endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif 
