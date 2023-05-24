18318d4548f8274238df2254c1fc527750f244b3
chengcli <chengcli@umich.edu>
2023-05-23 23:15:28 -0400

    add outputs

--- tools/check_style.sh
+++ tools/check_style.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-filters=-legal/copyright,-build/include_subdir,-build/include_order
+filters=-legal/copyright,-build/include_subdir,-build/include_order,-runtime/printf
 cpplint --filter=${filters} --recursive src/astro
 cpplint --filter=${filters} --recursive src/debugger
+
+for file in $(find src/outputs -name '*.cpp' -or -name '*.hpp' | grep -v "mppnccombine.cpp"); do
+    cpplint --filter=${filters} $file
+done
