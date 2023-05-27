66c39573806513c3fb39d4d423b974af9e4241dc
Cheng Li <chengcli@umich.edu>
2023-05-27 12:34:42 -0400

    wip

--- tools/kcoeff.cpp
+++ tools/kcoeff.cpp
@@ -1,98 +1,89 @@
 // C/C++
+#include <unistd.h>
+
 #include <cassert>
 #include <cstdlib>
-#include <unistd.h>
-#include <vector>
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 
 // canoe
 #include <configure.hpp>
 
 #ifdef NETCDFOUTPUT
 extern "C" {
-  #include "netcdf.h"
+#include "netcdf.h"
 }
 #endif
 
-template<typename T, int N>
+template <typename T, int N>
 class Array {
-protected:
-    int m_size[N];
-    std::vector<T> m_data;
+ protected:
+  int m_size[N];
+  std::vector<T> m_data;
 
-public:
-    Array() {};
+ public:
+  Array(){};
 
-    void resize(int n2, int n1) {
-        m_size[0] = n1;
-        m_size[1] = n2;
+  void resize(int n2, int n1) {
+    m_size[0] = n1;
+    m_size[1] = n2;
 
-        m_data.resize(n1 * n2);
-    }
+    m_data.resize(n1 * n2);
+  }
 
-    void resize(int n3, int n2, int n1) {
-        m_size[0] = n1;
-        m_size[1] = n2;
-        m_size[2] = n3;
+  void resize(int n3, int n2, int n1) {
+    m_size[0] = n1;
+    m_size[1] = n2;
+    m_size[2] = n3;
 
-        m_data.resize(n1 * n2 * n3);
-    }
+    m_data.resize(n1 * n2 * n3);
+  }
 
-    T* data() {
-        return m_data.data();
-    }
+  T* data() { return m_data.data(); }
 
-    T const* data() const {
-        return m_data.data();
-    }
+  T const* data() const { return m_data.data(); }
 
-    T& operator()(int i2, int i1) {
-        return m_data[i2 * m_size[0] + i1];
-    }
+  T& operator()(int i2, int i1) { return m_data[i2 * m_size[0] + i1]; }
 
-    T const& operator()(int i2, int i1) const {
-        return m_data[i2 * m_size[0] + i1];
-    }
+  T const& operator()(int i2, int i1) const {
+    return m_data[i2 * m_size[0] + i1];
+  }
 
-    T& operator()(int i3, int i2, int i1) {
-        return m_data[i3 * m_size[1] * m_size[0] + i2 * m_size[0] + i1];
-    }
+  T& operator()(int i3, int i2, int i1) {
+    return m_data[i3 * m_size[1] * m_size[0] + i2 * m_size[0] + i1];
+  }
 
-    T const& operator()(int i3, int i2, int i1) const {
-        return m_data[i3 * m_size[1] * m_size[0] + i2 * m_size[0] + i1];
-    }
+  T const& operator()(int i3, int i2, int i1) const {
+    return m_data[i3 * m_size[1] * m_size[0] + i2 * m_size[0] + i1];
+  }
 };
 
-bool fexists(std::string fname)
-{
-    std::ifstream ifile(fname.c_str());
-    return ifile.is_open();
+bool fexists(std::string fname) {
+  std::ifstream ifile(fname.c_str());
+  return ifile.is_open();
 }
 
-std::string decomment(
-        std::string fname
-        )
-{
-    if (!fexists(fname)) {
-        std::cout << fname << " does not exist." << std::endl;
-        assert(0);
+std::string decomment(std::string fname) {
+  if (!fexists(fname)) {
+    std::cout << fname << " does not exist." << std::endl;
+    assert(0);
+  }
+  // assert(fexists(fname));
+  std::ifstream file(fname.c_str(), std::ios::in);
+  std::string ss;
+  char c;
+  while (file) {
+    file.get(c);
+    if (c == '#') {
+      while (c != '\n' && file) file.get(c);
+      continue;
     }
-    //assert(fexists(fname));
-    std::ifstream file(fname.c_str(), std::ios::in);
-    std::string ss;
-    char c;
-    while (file) {
-        file.get(c);
-        if (c == '#') {
-            while (c != '\n' && file) file.get(c);
-            continue;
-        }
-        ss += c;
-    }
-    return ss;
+    ss += c;
+  }
+  return ss;
 }
 
 /** @file
@@ -102,7 +93,7 @@ std::string decomment(
  * Molecular absorption coefficients are calculated by RFM
  */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   clock_t cpu_tic = clock();
   std::cout << "kcoeff program running ..." << std::endl;
 
@@ -145,7 +136,6 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < nwaves; i++)
     wave_axis[i] = wmin + i * (wmax - wmin) / (nwaves - 1);
 
-
   // relative temperature
   inp >> tmin >> tmax >> ntemps;
   temp_axis.resize(ntemps);
@@ -158,21 +148,18 @@ int main(int argc, char *argv[]) {
   pres.resize(nlevels);
 
   // temperature profile
-  for (int i = 0; i < nlevels; i++)
-    inp >> temp[i];
+  for (int i = 0; i < nlevels; i++) inp >> temp[i];
 
   // pressure profile
   for (int i = 0; i < nlevels; i++) {
     inp >> pres[i];
-    pres[i] *= 100.; // mb -> pa
+    pres[i] *= 100.;  // mb -> pa
   }
-
 
   if (outfile.empty()) {
     std::cerr << "output netcdf file not specifeid." << std::endl;
     exit(1);
   }
-
 
 #ifdef NETCDFOUTPUT
 
@@ -182,19 +169,21 @@ int main(int argc, char *argv[]) {
 
   int wave_dimid, pres_dimid, *temp_dimid;
   int wave_varid, pres_varid, *temp_varid;
-  temp_dimid = new int [nmols];
-  temp_varid = new int [nmols];
+  temp_dimid = new int[nmols];
+  temp_varid = new int[nmols];
   nc_def_dim(ncid, "Wavenumber", wave_axis.size(), &wave_dimid);
   nc_def_var(ncid, "Wavenumber", NC_DOUBLE, 1, &wave_dimid, &wave_varid);
   nc_def_dim(ncid, "Pressure", pres.size(), &pres_dimid);
   nc_def_var(ncid, "Pressure", NC_DOUBLE, 1, &pres_dimid, &pres_varid);
   for (int i = 0; i < nmols; ++i) {
-    nc_def_dim(ncid, ("T_" + mols[i]).c_str(), temp_axis.size(), temp_dimid + i);
-    nc_def_var(ncid, ("T_" + mols[i]).c_str(), NC_DOUBLE, 1, temp_dimid + i, temp_varid + i);
+    nc_def_dim(ncid, ("T_" + mols[i]).c_str(), temp_axis.size(),
+               temp_dimid + i);
+    nc_def_var(ncid, ("T_" + mols[i]).c_str(), NC_DOUBLE, 1, temp_dimid + i,
+               temp_varid + i);
   }
 
   int tp_varid, *mol_varid;
-  mol_varid = new int [nmols];
+  mol_varid = new int[nmols];
   nc_def_var(ncid, "Temperature", NC_DOUBLE, 1, &pres_dimid, &tp_varid);
   for (int i = 0; i < nmols; i++) {
     int dims[3] = {wave_dimid, pres_dimid, temp_dimid[i]};
@@ -214,9 +203,10 @@ int main(int argc, char *argv[]) {
   std::ifstream infile;
   // molecule absorption
   kcoeff_mol.resize(nwaves, nlevels, ntemps);
-  //kcoeff_mol.NewAthenaArray(nwaves, nlevels, ntemps);
+  // kcoeff_mol.NewAthenaArray(nwaves, nlevels, ntemps);
   for (int m = 0; m < nmols; m++) {
-    std::cout << "making absorption file for " << mols[m] << " ..." << std::endl;
+    std::cout << "making absorption file for " << mols[m] << " ..."
+              << std::endl;
     infile.open(mfiles[m].c_str());
     if (infile.is_open()) {
       for (int i = 0; i < 10; i++) getline(infile, line);
@@ -224,7 +214,7 @@ int main(int argc, char *argv[]) {
         infile >> junk;
         for (int j = 0; j < nlevels; j++)
           for (int i = 0; i < ntemps; i++)
-              infile >> kcoeff_mol(k, nlevels - j - 1, i);
+            infile >> kcoeff_mol(k, nlevels - j - 1, i);
       }
       infile.close();
     } else {
@@ -234,19 +224,16 @@ int main(int argc, char *argv[]) {
     nc_put_var_double(ncid, temp_varid[m], temp_axis.data());
     nc_put_var_double(ncid, mol_varid[m], kcoeff_mol.data());
   }
-  //kcoeff_mol.DeleteAthenaArray();
+  // kcoeff_mol.DeleteAthenaArray();
 
   nc_close(ncid);
 
-#else // NO_NETCDFOUTPUT
+#else   // NO_NETCDFOUTPUT
   std::cout << "NO NETCDF, NO OUPUT" << std::endl;
 #endif  // NETCDFOUTPUT
 
   clock_t cpu_toc = clock();
   std::cout << "kcoeff program ends successfully in "
-       << double(cpu_toc - cpu_tic) / CLOCKS_PER_SEC
-       << " s." << std::endl
-       << "output file written into "
-       << outfile
-       << std::endl;
+            << double(cpu_toc - cpu_tic) / CLOCKS_PER_SEC << " s." << std::endl
+            << "output file written into " << outfile << std::endl;
 }
