87685c1d9492a91939abfdbc4a695ef6fadc91c1
Cheng Li <chengcli@umich.edu>
2023-05-27 21:08:49 -0400

    secrets

--- cmake/rfm.cmake
+++ cmake/rfm.cmake
@@ -1,8 +1,15 @@
 include(FetchContent)
 
 set(FETCHCONTENT_QUIET FALSE)
-set(GITHUB_USER $ENV{GH_ACCOUNT})
-set(GITHUB_PAT $ENV{GH_TOKEN})
+
+macro(set_if_empty _variable)
+  if("${${_variable}}" STREQUAL "")
+    set(${_variable} ${ARGN})
+  endif()
+endmacro()
+
+set_if_empty(ACCOUNT $ENV{GH_ACCOUNT})
+set_if_empty(TOKEN $ENV{GH_TOKEN})
 
 option(RFM "Build RFM" ON)
 
@@ -10,7 +17,7 @@ if(RFM)
   FetchContent_Declare(
     rfm
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    URL https://${GITHUB_USER}:${GITHUB_PAT}@github.com/luminoctum/rfm/archive/refs/tags/v4.33.tar.gz
+    URL https://${ACCOUNT}:${TOKEN}@github.com/luminoctum/rfm/archive/refs/tags/v4.33.tar.gz
   )
 
   FetchContent_GetProperties(rfm)
