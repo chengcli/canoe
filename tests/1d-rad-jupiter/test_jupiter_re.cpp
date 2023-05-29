fbc85522d970a025bd6941827e1b467e6a40fe4e
Cheng Li <chengcli@umich.edu>
2023-05-29 13:39:45 -0400

    add test

--- tests/1d-rad-jupiter/test_jupiter_re.cpp
+++ tests/1d-rad-jupiter/test_jupiter_re.cpp
@@ -217,3 +217,164 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   delete[] t1;
   pdebug->Leave();
 }
+
+int main(int argc, char **argv) {
+  static_assert(HYDROSTATIC,
+                "This problem requires turning on hydrostatic option");
+  std::stringstream msg;
+
+  // no MPI
+  Globals::my_rank = 0;
+  Globals::nranks = 1;
+
+  IOWrapper infile;
+  infile.Open("saturn_atmosphere.inp", IOWrapper::FileMode::read);
+
+  ParameterInput *pinput = new ParameterInput;
+  pinput->LoadFromFile(infile);
+  infile.Close();
+
+  // set up mesh
+  int restart = false;
+  int mesh_only = false;
+
+  pdebug = std::make_unique<Debugger>(1);
+
+  Mesh *pmesh = new Mesh(pinput, mesh_only);
+
+  // set up components
+  for (int b = 0; b < pmesh->nblocal; ++b) {
+    MeshBlock *pmb = pmesh->my_blocks(b);
+    pmb->pimpl = std::make_shared<MeshBlock::Impl>(pmb, pinput);
+  }
+
+  // initialize mesh
+  pmesh->Initialize(restart, pinput);
+
+  // mesh limits
+  Real x1min = pmesh->mesh_size.x1min;
+  Real x1max = pmesh->mesh_size.x1max;
+  Real H0 = Constants::PressureScaleHeight;
+
+  // request temperature and pressure
+  pdebug->Message("request T", T0);
+  pdebug->Message("request P", P0);
+
+  // thermodynamic constants
+  Real gamma = pinput->GetReal("hydro", "gamma");
+  Real Rd = pinput->GetReal("thermodynamics", "Rd");
+  Real cp = gamma / (gamma - 1.) * Rd;
+  Real Tmin = pinput->GetReal("problem", "Tmin");
+
+  // estimate surface temperature and pressure
+  Real Ps = P0 * exp(-x1min / H0);
+  Real Ts = T0 * pow(Ps / P0, Rd / cp);
+  Real dlnp, **w1, *z1, *t1;
+  dlnp = (x1max - x1min) / pmesh->mesh_size.nx1 / (2. * H0);
+  int nx1 = (int)((x1max - x1min) / (H0 * dlnp));
+  NewCArray(w1, nx1, NumHydros + 2 * NumVapors);
+  z1 = new Real[nx1];
+  t1 = new Real[nx1];
+
+  // read in vapors
+  w1[0][iH2O] = pinput->GetReal("problem", "qH2O");
+  w1[0][iNH3] = pinput->GetReal("problem", "qNH3");
+  z1[0] = x1min;
+  for (int i = 1; i < nx1; ++i) z1[i] = z1[i - 1] + H0 * dlnp;
+
+  // set up variables
+  for (int b = 0; b < pmesh->nblocal; ++b) {
+    MeshBlock *pmb = pmesh->my_blocks(b);
+    auto phydro = pmb->phydro;
+    auto pfield = pmb->pfield;
+    auto pcoord = pmb->pcoord;
+    auto peos = pmb->peos;
+    auto pthermo = pmb->pimpl->pthermo;
+
+    // set up an adiabatic atmosphere
+    int max_iter = 200, iter = 0;
+    Real t0;
+    while (iter++ < max_iter) {
+      pthermo->ConstructAtmosphere(w1, Ts, Ps, grav, -dlnp, nx1,
+                                   Adiabat::pseudo, 1.);
+
+      // Replace adiabatic atmosphere with isothermal atmosphere if temperature
+      // is too low
+      int ii = 0;
+      for (; ii < nx1 - 1; ++ii)
+        if (pthermo->GetTemp(w1[ii]) < Tmin) break;
+      Real Tv = w1[ii][ipr] / (w1[ii][idn] * Rd);
+      for (int i = ii; i < nx1; ++i) {
+        w1[i][idn] = w1[i][ipr] / (Rd * Tv);
+        for (int n = 1; n <= NumVapors; ++n) w1[i][n] = w1[ii][n];
+      }
+
+      // Find T at p = p0
+      for (int i = 0; i < nx1; ++i) {
+        t1[i] = pthermo->GetTemp(w1[i]);
+      }
+      t0 = interp1(0, t1, z1, nx1);
+
+      Ts += T0 - t0;
+      if (fabs(T0 - t0) < 0.01) break;
+
+      pdebug->Message("iteration #", iter);
+      pdebug->Message("T", t0);
+    }
+
+    if (iter > max_iter) {
+      msg << "### FATAL ERROR" << std::endl
+          << "maximum iteration reached." << std::endl
+          << "T0 = " << t0;
+      ATHENA_ERROR(msg);
+    }
+
+    // Change to log quantity
+    for (int i = 0; i < nx1; ++i)
+      for (int n = 0; n < NumHydros + 2 * NumVapors; ++n) {
+        if (n == iv1 || n == iv2 || n == iv3)
+          w1[i][n] = 0.;
+        else
+          w1[i][n] = log(w1[i][n]);
+      }
+
+    // interpolate to model grid
+    int is = pmb->is, js = pmb->js, ks = pmb->ks;
+    int ie = pmb->ie, je = pmb->je, ke = pmb->ke;
+    for (int k = ks; k <= ke; ++k)
+      for (int j = js; j <= je; ++j)
+        for (int i = is; i <= ie; ++i) {
+          Real buf[NumHydros + 2 * NumVapors];
+
+          // set gas concentration and velocity
+          interpn(buf, &pcoord->x1v(i), *w1, z1, &nx1, 1,
+                  NumHydros + 2 * NumVapors);
+          for (int n = 0; n < NumHydros; ++n)
+            for (int k = ks; k <= ke; ++k)
+              for (int j = js; j <= je; ++j) {
+                if (n == iv1 || n == iv2 || n == iv3)
+                  phydro->w(n, k, j, i) = 0.;
+                else {
+                  phydro->w(n, k, j, i) = exp(buf[n]);
+                }
+              }
+        }
+
+    // primitive to conserved conversion
+    peos->PrimitiveToConserved(phydro->w, pfield->bcc, phydro->u, pmb->pcoord,
+                               is, ie, js, je, ks, ke);
+  }
+
+  // make output
+  Outputs *pouts;
+  pouts = new Outputs(pmesh, pinput);
+  pouts->MakeOutputs(pmesh, pinput);
+
+  FreeCArray(w1);
+  delete[] z1;
+  delete[] t1;
+
+  delete pinput;
+  delete pmesh;
+  delete pouts;
+}
