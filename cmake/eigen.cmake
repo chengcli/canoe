fd5731b44bbce53874229aa8954afba32552622b
Cheng Li <69489965+chengcli@users.noreply.github.com>
2023-06-05 01:00:02 -0400

    Upgrade cmake (#32)
    remove fine-grained libs

--- cmake/eigen.cmake
+++ cmake/eigen.cmake
@@ -12,19 +12,6 @@ set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 set(EIGEN_BUILD_DOC OFF)
 set(EIGEN_BUILD_PKGCONFIG OFF)
 
-if(NOT eigen_POPULATED)
-  FetchContent_Populate(eigen)
-  add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR})
-endif()
+FetchContent_MakeAvailable(eigen)
 
-set(EIGEN_INCLUDE_DIR
-    ${Eigen3_SOURCE_DIR}
-    CACHE PATH "Eigen include directory")
-
-# find_package(Eigen3 3.3 REQUIRED NO_MODULE) include(FetchContent)
-# FetchContent_Declare( Eigen GIT_REPOSITORY
-# https://gitlab.com/libeigen/eigen.git GIT_TAG master GIT_SHALLOW TRUE
-# GIT_PROGRESS TRUE)
-
-# set(CMAKE_POLICY_DEFAULT_CMP0077 NEW) set(EIGEN_BUILD_DOC OFF)
-# set(EIGEN_BUILD_PKGCONFIG OFF) FetchContent_MakeAvailable(Eigen)
+include_directories(${Eigen3_SOURCE_DIR})
