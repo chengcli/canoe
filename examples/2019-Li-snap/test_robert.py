5a1beb884d256f653547729690219fa4f87767cb
chengcli <chengcli@umich.edu>
2023-07-02 01:25:13 -0400

    add straka and robert

---
+++ examples/2019-Li-snap/test_robert.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+from netCDF4 import Dataset
+from numpy import sum, sqrt
+import sys
+
+case1 = sys.argv[1]
+case2 = sys.argv[2]
+
+data1 = Dataset(case1, 'r')
+theta1 = data1['theta'][-1,:,:,:]
+
+data2 = Dataset(case2, 'r')
+theta2 = data2['theta'][-1,:,:,:]
+
+diff = sqrt(sum((theta2 - theta1)*(theta2 - theta1)))
+
+if diff < 1.E-3:
+  print('### robert test passed. ###')
+else:
+  raise ValueError('ERROR: robert test failed. L2-norm is %.2g' % diff)
