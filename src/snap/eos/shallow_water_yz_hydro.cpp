8ae264e99c9ab2c687107d3b8263722ac4c739c1
chengcli <chengcli@umich.edu>
2023-07-11 10:41:04 -0400

    wip

---
+++ src/snap/eos/shallow_water_yz_hydro.cpp
@@ -0,0 +1,111 @@
+//! \file shallow_water.cpp
+//  \brief implements functions in class EquationOfState for shallow water
+//  hydrodynamics
+
+// C/C++ headers
+#include <cmath>  // sqrt()
+
+// Athena++ headers
+#include <eos/eos.hpp>
+// #include <hydro/hydro.hpp>
+#include <athena.hpp>
+#include <field/field.hpp>
+#include <mesh/mesh.hpp>
+#include <parameter_input.hpp>
+
+// EquationOfState constructor
+//
+EquationOfState::EquationOfState(MeshBlock *pmb, ParameterInput *pin)
+    : pmy_block_(pmb),
+      density_floor_{
+          pin->GetOrAddReal("hydro", "dfloor", std::sqrt(1024 * float_min))},
+      pressure_floor_{
+          pin->GetOrAddReal("hydro", "pfloor", std::sqrt(1024 * float_min))} {}
+
+//----------------------------------------------------------------------------------------
+// \!fn void EquationOfState::ConservedToPrimitive(AthenaArray<Real> &cons,
+//           const AthenaArray<Real> &prim_old, const FaceField &b,
+//           AthenaArray<Real> &prim, AthenaArray<Real> &bcc, Coordinates *pco,
+//           int il, int iu, int jl, int ju, int kl, int ku)
+// \brief Converts conserved into primitive variables in shallow water.
+
+void EquationOfState::ConservedToPrimitive(
+    AthenaArray<Real> &cons, const AthenaArray<Real> &prim_old,
+    const FaceField &b, AthenaArray<Real> &prim, AthenaArray<Real> &bcc,
+    Coordinates *pco, int il, int iu, int jl, int ju, int kl, int ku) {
+  for (int k = kl; k <= ku; ++k)
+    for (int j = jl; j <= ju; ++j)
+      for (int i = il; i <= iu; ++i) {
+        Real &ud = cons(IDN, k, j, i);
+        Real &wd = prim(IDN, k, j, i);
+
+        // apply density floor, without changing momentum or energy
+        ud = (ud > density_floor_) ? ud : density_floor_;
+        wd = ud;
+
+        Real di = 1. / ud;
+        prim(IVX, k, j, i) = 0.;
+        prim(IVY, k, j, i) = cons(IVY, k, j, i) * di;
+        prim(IVZ, k, j, i) = cons(IVZ, k, j, i) * di;
+      }
+}
+
+//----------------------------------------------------------------------------------------
+// \!fn void EquationOfState::PrimitiveToConserved(const AthenaArray<Real>
+// &prim,
+//           const AthenaArray<Real> &bc, AthenaArray<Real> &cons, Coordinates
+//           *pco, int il, int iu, int jl, int ju, int kl, int ku);
+// \brief Converts primitive variables into conservative variables
+
+void EquationOfState::PrimitiveToConserved(const AthenaArray<Real> &prim,
+                                           const AthenaArray<Real> &bc,
+                                           AthenaArray<Real> &cons,
+                                           Coordinates *pco, int il, int iu,
+                                           int jl, int ju, int kl, int ku) {
+  for (int k = kl; k <= ku; ++k)
+    for (int j = jl; j <= ju; ++j)
+      for (int i = il; i <= iu; ++i) {
+        const Real &wd = prim(IDN, k, j, i);
+        cons(IDN, k, j, i) = wd;
+        cons(IVX, k, j, i) = 0.;
+        cons(IVY, k, j, i) = prim(IVY, k, j, i) * wd;
+        cons(IVZ, k, j, i) = prim(IVZ, k, j, i) * wd;
+      }
+}
+
+//----------------------------------------------------------------------------------------
+// \!fn Real EquationOfState::SoundSpeed(Real prim[NHYDRO])
+// \brief returns shallow water gravity wave speed given vector of primitive
+// variables
+
+Real EquationOfState::SoundSpeed(const Real prim[NHYDRO]) {
+  return std::sqrt(prim[IDN]);
+}
+
+//---------------------------------------------------------------------------------------
+// \!fn void EquationOfState::ApplyPrimitiveFloors(AthenaArray<Real> &prim,
+//           int k, int j, int i)
+// \brief Apply density and pressure floors to reconstructed L/R cell interface
+// states
+
+void EquationOfState::ApplyPrimitiveFloors(AthenaArray<Real> &prim, int k,
+                                           int j, int i) {
+  Real &w_d = prim(IDN, i);
+
+  // apply density floor
+  w_d = (w_d > density_floor_) ? w_d : density_floor_;
+
+  return;
+}
+
+void EquationOfState::ApplyPrimitiveConservedFloors(AthenaArray<Real> &prim,
+                                                    AthenaArray<Real> &cons,
+                                                    AthenaArray<Real> &bcc,
+                                                    int k, int j, int i) {
+  Real &w_d = prim(IDN, k, j, i);
+  Real &u_d = cons(IDN, k, j, i);
+
+  // apply density floor
+  w_d = (w_d > density_floor_) ? w_d : density_floor_;
+  u_d = w_d;
+}
---
+++ src/snap/riemann/lmars_shallow_water_yz.cpp
@@ -0,0 +1,76 @@
+//! \file  lmars_shallow_water.cpp
+//  \brief
+
+// C/C++ headers
+#include <algorithm>  // min()
+#include <cmath>      // sqrt()
+#include <iostream>
+
+// Athena++ headers
+#include <athena.hpp>
+#include <coordinates/coordinates.hpp>
+#include <hydro/hydro.hpp>
+#include <mesh/mesh.hpp>
+
+void Hydro::RiemannSolver(int const k, int const j, int const il, int const iu,
+                          int const ivx, AthenaArray<Real> &wl,
+                          AthenaArray<Real> &wr, AthenaArray<Real> &flx,
+                          AthenaArray<Real> const &dxw) {
+  int ivy = ivx == IVY ? IVZ : IVY;
+
+  Real wli[NHYDRO], wri[NHYDRO];
+
+  Real ubar, cbar, hbar;
+
+  AthenaArray<Real> empty{};  // placeholder for unused electric/magnetic fields
+#ifdef AFFINE                 // need of projection
+  {
+    switch (ivx) {
+      case IVY:
+        pmy_block->pcoord->PrimToLocal2(k, j, il, iu, empty, wl, wr, empty);
+        break;
+      case IVZ:
+        pmy_block->pcoord->PrimToLocal3(k, j, il, iu, empty, wl, wr, empty);
+        break;
+    }
+  }
+#endif  // AFFINE
+
+  for (int i = il; i <= iu; ++i) {
+    for (int n = 0; n < NHYDRO; ++n) {
+      wli[n] = wl(n, i);
+      wri[n] = wr(n, i);
+    }
+
+    cbar = sqrt(0.5 * (wli[IDN] + wri[IDN]));
+    hbar = 0.5 * (wli[IDN] + wri[IDN]) + 0.5 * cbar * (wli[ivx] - wri[ivx]);
+    ubar = 0.5 * (wli[ivx] + wri[ivx]) + 0.5 / cbar * (wli[IDN] - wri[IDN]);
+
+    if (ubar > 0.) {
+      flx(IDN, k, j, i) = ubar * wli[IDN];
+      flx(ivx, k, j, i) = ubar * wli[IDN] * wli[ivx] + 0.5 * hbar * hbar;
+      flx(ivy, k, j, i) = ubar * wli[IDN] * wli[ivy];
+    } else {
+      flx(IDN, k, j, i) = ubar * wri[IDN];
+      flx(ivx, k, j, i) = ubar * wri[IDN] * wri[ivx] * 0.5 * hbar * hbar;
+      flx(ivy, k, j, i) = ubar * wri[IDN] * wri[ivy];
+    }
+    flx(IVX, k, j, i) = 0.;
+  }
+
+#ifdef AFFINE  // need of deprojection
+  {
+    // check if this is correct
+    switch (ivx) {
+      case IVY:
+        pmy_block->pcoord->FluxToGlobal2(k, j, il, iu, empty, empty, flx, empty,
+                                         empty);
+        break;
+      case IVZ:
+        pmy_block->pcoord->FluxToGlobal3(k, j, il, iu, empty, empty, flx, empty,
+                                         empty);
+        break;
+    }
+  }
+#endif  // AFFINE
+}
