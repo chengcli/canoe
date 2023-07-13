e39c384bd26f47011b445fd453f0baf6a8290225
chengcli <chengcli@umich.edu>
2023-07-12 23:47:08 -0400

    wip

--- cmake/disort.cmake
+++ cmake/disort.cmake
@@ -17,7 +17,7 @@ if(DISORT)
   FetchContent_Declare(
     pydisort
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    URL https://${ACCOUNT}:${TOKEN}@github.com/zoeyzyhu/pydisort/archive/refs/tags/v0.4.0.tar.gz
+    URL https://${ACCOUNT}:${TOKEN}@github.com/zoeyzyhu/pydisort/archive/refs/tags/v0.4.1.tar.gz
   )
 
   FetchContent_MakeAvailable(pydisort)
--- src/harp/rt_solver_disort.cpp
+++ src/harp/rt_solver_disort.cpp
@@ -21,11 +21,12 @@
 
 // harp
 #include "radiation.hpp"
+#include "radiation_utils.hpp"
 #include "rt_solvers.hpp"
 
 void RadiationBand::RTSolverDisort::CalBandFlux(Direction const &rayInput,
-                                                Real dist, int k, int j, int il,
-                                                int iu) {
+                                                Real dist_au, int k, int j,
+                                                int il, int iu) {
   auto pcomm = Communicator::GetInstance();
   auto pmb = pmy_band_->pmy_block_;
   auto pcoord = pmb->pcoord;
@@ -45,34 +46,36 @@ void RadiationBand::RTSolverDisort::CalBandFlux(Direction const &rayInput,
   auto &tau = pmy_band_->tau_;
   auto &ssa = pmy_band_->ssa_;
   auto &pmom = pmy_band_->pmom_;
+  auto &flxup = pmy_band_->flxup_;
+  auto &flxdn = pmy_band_->flxdn_;
 
-  if (ds.flag.ibcnd != 0) {
-    throw ValuError("RTSolverDisort::CalRadtranFlux", "ibcnd", ds.flag.ibcnd,
-                    0);
+  if (ds_.flag.ibcnd != 0) {
+    throw ValueError("RTSolverDisort::CalRadtranFlux", "ibcnd", ds_.flag.ibcnd,
+                     0);
   }
   pcomm->SetColor(pmb, X1DIR);
 
   int nblocks = pmb->pmy_mesh->mesh_size.nx1 / pmb->block_size.nx1;
-  Real *bufrecv = new Real[(iu - il) * nblocks * (ds.nmom_nstr + 3)];
+  Real *bufrecv = new Real[(iu - il) * nblocks * (ds_.nmom_nstr + 3)];
 
-  if (ds.flag.planck) {
-    pcomm->GatherData(temf_ + il, bufrecv, iu - il + 1);
+  if (ds_.flag.planck) {
+    pcomm->GatherData(&temf(il), bufrecv, iu - il + 1);
 
     for (int i = 0; i < (iu - il + 1) * nblocks; ++i) {
       int m = i / (iu - il + 1);
-      ds.temper[m * (iu - il) + i % (iu - il + 1)] = bufrecv[i];
+      ds_.temper[m * (iu - il) + i % (iu - il + 1)] = bufrecv[i];
     }
 
-    std::reverse(ds.temper, ds.temper + ds.nlyr + 1);
+    std::reverse(ds_.temper, ds_.temper + ds_.nlyr + 1);
   }
-  // for (int i = 0; i <= ds.nlyr; ++i)
-  //   std::cout << ds.temper[i] << std::endl;
+  // for (int i = 0; i <= ds_.nlyr; ++i)
+  //   std::cout << ds_.temper[i] << std::endl;
 
-  ds.bc.umu0 = rayInput.mu > 1.E-3 ? rayInput.mu : 1.E-3;
-  ds.bc.phi0 = rayInput.phi;
-  if (ds.flag.planck) {
-    ds.bc.btemp = ds.temper[ds.nlyr];
-    ds.bc.ttemp = ds.temper[0];
+  ds_.bc.umu0 = rayInput.mu > 1.E-3 ? rayInput.mu : 1.E-3;
+  ds_.bc.phi0 = rayInput.phi;
+  if (ds_.flag.planck) {
+    ds_.bc.btemp = ds_.temper[ds_.nlyr];
+    ds_.bc.ttemp = ds_.temper[0];
   }
 
   // reset flx of this column
@@ -85,10 +88,10 @@ void RadiationBand::RTSolverDisort::CalBandFlux(Direction const &rayInput,
   if (bflags & RadiationFlags::CorrelatedK) {
     // stellar source function
     if (bflags & RadiationFlags::Star)
-      ds.bc.fbeam = planet->ParentInsolationFlux(wmin, wmax, dist_au);
+      ds_.bc.fbeam = planet->ParentInsolationFlux(wmin, wmax, dist_au);
     // planck source function
-    ds.wvnmlo = wmin;
-    ds.wvnmhi = wmax;
+    ds_.wvnmlo = wmin;
+    ds_.wvnmhi = wmax;
   }
 
   int r = pcomm->GetRank(pmb, X1DIR);
@@ -102,37 +105,37 @@ void RadiationBand::RTSolverDisort::CalBandFlux(Direction const &rayInput,
     if (!(bflags & RadiationFlags::CorrelatedK)) {
       // stellar source function
       if (bflags & RadiationFlags::Star)
-        ds.bc.fbeam =
+        ds_.bc.fbeam =
             planet->ParentInsolationFlux(spec[n].wav1, spec[n].wav2, dist_au);
       // planck source function
-      ds.wvnmlo = spec[n].wav1;
-      ds.wvnmhi = spec[n].wav2;
+      ds_.wvnmlo = spec[n].wav1;
+      ds_.wvnmhi = spec[n].wav2;
     }
 
     // pack data
     packSpectralProperties(bufsend, &tau(n, il), &ssa(n, il), &pmom(n, il, 0),
                            iu - il, npmom + 1);
     pcomm->GatherData(bufsend, bufrecv, dsize);
-    unpackSpectralProperties(ds.dtauc, ds.ssalb, ds.pmom, bufrecv, iu - il,
-                             npmom + 1, nblocks, ds.nmom_nstr + 1);
+    unpackSpectralProperties(ds_.dtauc, ds_.ssalb, ds_.pmom, bufrecv, iu - il,
+                             npmom + 1, nblocks, ds_.nmom_nstr + 1);
 
     // absorption
-    std::reverse(ds.dtauc, ds.dtauc + ds.nlyr);
+    std::reverse(ds_.dtauc, ds_.dtauc + ds_.nlyr);
 
     // single scatering albedo
-    std::reverse(ds.ssalb, ds.ssalb + ds.nlyr);
+    std::reverse(ds_.ssalb, ds_.ssalb + ds_.nlyr);
 
     // Legendre coefficients
-    std::reverse(ds.pmom, ds.pmom + ds.nlyr * (ds.nmom_nstr + 1));
-    for (int i = 0; i < ds.nlyr; ++i)
-      std::reverse(ds.pmom + i * (ds.nmom_nstr + 1),
-                   ds.pmom + (i + 1) * (ds.nmom_nstr + 1));
+    std::reverse(ds_.pmom, ds_.pmom + ds_.nlyr * (ds_.nmom_nstr + 1));
+    for (int i = 0; i < ds_.nlyr; ++i)
+      std::reverse(ds_.pmom + i * (ds_.nmom_nstr + 1),
+                   ds_.pmom + (i + 1) * (ds_.nmom_nstr + 1));
 
     // run disort
-    c_disort(&ds, &ds_out);
+    c_disort(&ds_, &ds_out_);
 
     // Counting index
-    // Example, il = 0, iu = 2, ds.nlyr = 6, partition in to 3 blocks
+    // Example, il = 0, iu = 2, ds_.nlyr = 6, partition in to 3 blocks
     // face id   -> 0 - 1 - 2 - 3 - 4 - 5 - 6
     // cell id   -> | 0 | 1 | 2 | 3 | 4 | 5 |
     // disort id -> 6 - 5 - 4 - 3 - 2 - 1 - 0
@@ -147,20 +150,20 @@ void RadiationBand::RTSolverDisort::CalBandFlux(Direction const &rayInput,
     // block r = 2 gets, 2 - 1 - 0
     // accumulate flux from lines
     for (int i = il; i <= iu; ++i) {
-      int m = ds.nlyr - (r * (iu - il) + i - il);
+      int m = ds_.nlyr - (r * (iu - il) + i - il);
       /*! \bug does not work for spherical geometry, need to scale area using
        * farea(il)/farea(i)
        */
       // flux up
-      flxup_(n, i) = ds_out.rad[m].flup;
+      flxup(n, i) = ds_out_.rad[m].flup;
 
       /*! \bug does not work for spherical geomtry, need to scale area using
        * farea(il)/farea(i)
        */
       // flux down
-      flxdn_(n, i) = ds_out.rad[m].rfldir + ds_out.rad[m].rfldn;
-      bflxup(k, j, i) += spec[n].wght * flxup_(n, i);
-      bflxdn(k, j, i) += spec[n].wght * flxdn_(n, i);
+      flxdn(n, i) = ds_out_.rad[m].rfldir + ds_out_.rad[m].rfldn;
+      bflxup(k, j, i) += spec[n].wght * flxup(n, i);
+      bflxdn(k, j, i) += spec[n].wght * flxdn(n, i);
     }
 
     // spherical correction by XIZ
