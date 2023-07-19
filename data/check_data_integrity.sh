36a0ac5ffad1f8ef0bcfaeacd2a920a0c354d22a
Cheng Li <69489965+chengcli@users.noreply.github.com>
2023-07-19 17:24:57 -0400

    Fix RFM 5.20 issues (#52)
    Finishing upgrading RFM

--- data/check_data_integrity.sh
+++ data/check_data_integrity.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-$1/fetch_hitran2012.sh $1
+#$1/fetch_hitran2012.sh $1
+$1/fetch_hitran2020.sh $1
 $1/fetch_jup_midir_H2broaden.sh $1
 $1/fetch_jup_atm_moses_modelc.sh $1
 $1/fetch_H2-He-cia.sh $1
