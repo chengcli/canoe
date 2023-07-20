a68b91afe1555d3e62b1413c047e44ddac87311c
Cheng Li <chengcli@umich.edu>
2023-07-20 10:34:31 -0400

    add sw actions

--- examples/2020-Li-polar-vortex/test_shallow.py
+++ examples/2020-Li-polar-vortex/test_shallow.py
@@ -14,7 +14,7 @@ var2 = data2['rho'][-1,:,:,0]
 
 diff = sqrt(sum((var2 - var1)*(var2 - var1)))
 
-if diff < 1.E-6:
+if diff < 1.E-5:
   print('### shallow water test passed. ###')
 else:
   raise ValueError('ERROR: shallow water test failed. L2-norm is %.2g' % diff)
