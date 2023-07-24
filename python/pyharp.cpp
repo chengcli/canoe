fa2d31172a51e152c9bbe1e31b14f1edd5cd6e75
chengcli <chengcli@umich.edu>
2023-07-23 20:43:15 -0400

    wip

---
+++ python/pyharp.cpp
@@ -0,0 +1,19 @@
+// pybind
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+// harp
+#include <harp/radiation.hpp>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pyharp, m) {
+  py::class_<Radiation>(m, "radiation")
+      .def_read("radiance", &Radiation::radiance)
+      .def_read("fluxup", &Radiation::flxup)
+      .def_read("fluxdn", &Radiation::flxdn)
+
+      .def(py::init())
+
+      .def("populate_radiation_bands", &Radiation::PopulateRadiationBands);
+}
