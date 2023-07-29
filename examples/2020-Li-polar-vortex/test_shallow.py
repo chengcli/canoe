0cebd2df412ec3b8b92895e77e16f0f078f65b50
chengcli <chengcli@umich.edu>
2023-07-28 21:06:18 -0400

    format

--- examples/2020-Li-polar-vortex/test_shallow.py
+++ examples/2020-Li-polar-vortex/test_shallow.py
@@ -6,15 +6,15 @@ import sys
 case1 = sys.argv[1]
 case2 = sys.argv[2]
 
-data1 = Dataset(case1, 'r')
-var1 = data1['rho'][-1,:,:,0]
+data1 = Dataset(case1, "r")
+var1 = data1["rho"][-1, :, :, 0]
 
-data2 = Dataset(case2, 'r')
-var2 = data2['rho'][-1,:,:,0]
+data2 = Dataset(case2, "r")
+var2 = data2["rho"][-1, :, :, 0]
 
-diff = sqrt(sum((var2 - var1)*(var2 - var1)))
+diff = sqrt(sum((var2 - var1) * (var2 - var1)))
 
-if diff < 1.E-5:
-  print('### shallow water test passed. ###')
+if diff < 1.0e-5:
+    print("### shallow water test passed. ###")
 else:
-  raise ValueError('ERROR: shallow water test failed. L2-norm is %.2g' % diff)
+    raise ValueError("ERROR: shallow water test failed. L2-norm is %.2g" % diff)
