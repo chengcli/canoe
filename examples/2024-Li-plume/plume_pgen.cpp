f5e3164178efadc97bebb7463c81faaf1bf8357d
chengcli <chengcli@umich.edu>
2023-08-10 20:43:05 -0400

    wip

--- examples/2024-Li-plume/plume_pgen.cpp
+++ examples/2024-Li-plume/plume_pgen.cpp
@@ -1,15 +1,3 @@
-/* -------------------------------------------------------------------------------------
- * SNAP Example Program
- *
- * Contributer:
- * Cheng Li, University of Michigan
- *
- * Year: 2023
- * Contact: chengcli@umich.edu
- * Reference: Bryan and Fritsch, 2002
- * -------------------------------------------------------------------------------------
- */
-
 // athena
 #include <athena/athena.hpp>
 #include <athena/athena_arrays.hpp>
@@ -77,24 +65,6 @@ void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin) {
         pimpl->GatherFromPrimitive(&air, k, j, i);
         user_out_var(6, k, j, i) = air.w[iH2O] + air.c[iH2Oc];
       }
-}
-
-double sat_vapor_p_H2O(double T) {
-  double betal = 24.845, gammal = -2.1735, tr = 273.16, pr = 611.7;
-  return SatVaporPresIdeal(T / tr, pr, betal, gammal);
-}
-
-// water svp
-void Thermodynamics::enrollVaporFunctionH2O() {
-  Application::Logger app("snap");
-  app->Log("Enrolling H2O vapor pressures");
-
-  auto pindex = IndexMap::GetInstance();
-  int iH2O = pindex->GetVaporId("H2O");
-
-  svp_func1_[iH2O][0] = [](AirParcel const &qfrac, int, int) {
-    return sat_vapor_p_H2O(qfrac.w[IDN]);
-  };
 }
 
 void CondensateGravity(MeshBlock *pmb, Real const time, Real const dt,
