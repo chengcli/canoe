143ba9ffe0ceaa6cc9e8b397ecab76310e7da55c
Cheng Li <69489965+chengcli@users.noreply.github.com>
2023-08-14 10:39:36 -0400

    Upload artifact (#83)
    Mechanism is here but the it does not work

--- tools/check_artifact.sh
+++ tools/check_artifact.sh
@@ -7,19 +7,24 @@ ARTIFACT_NAME=$1
 # GitHub repository, like user/repo
 REPO_NAME=canoe
 # GitHub API token for authentication
-TOKEN=none
+TOKEN=$2
 
 # Fetch artifact details using GitHub API
-ARTIFACT_URL=$(curl -s -H "Authorization: token $TOKEN" \
-  "https://api.github.com/repos/$REPO_NAME/actions/artifacts" \
+ARTIFACT_JSON=$(curl -s -H "Authorization: token $TOKEN" \
+  "https://api.github.com/repos/chengcli/canoe/actions/artifacts")
+
+# Check if the artifacts list is null
+if [[ $(echo "$ARTIFACT_JSON" | jq '.artifacts') == "null" ]]; then
+  echo "false"
+else
+  ARTIFACT_URL=$(echo "$ARTIFACT_JSON" \
   | jq -r --arg ARTIFACT_NAME "$ARTIFACT_NAME" \
   '.artifacts[] | select(.name==$ARTIFACT_NAME) | .url')
 
-# Check and set environment variable
-if [[ -z "$ARTIFACT_URL" ]]; then
-  echo "Artifact not found."
-  echo "false"
-else
-  echo "Artifact found!"
-  echo "true"
+  # Check and set environment variable
+  if [[ -z "$ARTIFACT_URL" ]]; then
+    echo "false"
+  else
+    echo "true"
+  fi
 fi
