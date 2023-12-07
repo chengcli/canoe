8e2402bc391cd533c837428cbb6ad964661080b0
Linfeng Li <linfel@umich.edu>
2023-12-07 14:27:28 -0500

    lavaplanet input and cpp files

---
+++ cmake/lava.cmake
@@ -0,0 +1,22 @@
+# configure file for hydrogen-water world
+
+macro(SET_IF_EMPTY _variable)
+  if("${${_variable}}" STREQUAL "")
+    set(${_variable} ${ARGN})
+  endif()
+endmacro()
+
+# athena variables
+set_if_empty(NUMBER_GHOST_CELLS 3)
+
+# canoe configure
+set(NVAPOR 1)
+set(NCLOUD 2)
+set(NPHASE_LEGACY 3)
+set(NETCDF ON)
+set(PNETCDF ON)
+set(MPI ON)
+set(DISORT ON)
+set(PYTHON_BINDINGS ON)
+set(TASKLIST ImplicitHydroTasks)
+set(RSOLVER lmars)
