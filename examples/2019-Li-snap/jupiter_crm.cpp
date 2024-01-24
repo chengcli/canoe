e1a5d25f5391df729185931173e1a3093fcb805d
mac/cli <chengcli@umich.edu>
2024-01-24 12:18:39 -0500

    polar vortex

--- examples/2019-Li-snap/jupiter_crm.cpp
+++ examples/2019-Li-snap/jupiter_crm.cpp
@@ -54,6 +54,15 @@ void MeshBlock::InitUserMeshBlockData(ParameterInput *pin) {
   }
 }
 
+void FindLatlon(Real *lat, Real *lon, Real x2, Real x1) {
+  Real Rp = 7E7;
+  Real dist = sqrt(x1 * x1 + x2 * x2);
+  *lat = M_PI / 2. - dist / Rp;
+  *lon = asin(x1 / dist);
+  if (x2 > 0 && x1 > 0) *lon = M_PI - *lon;
+  if (x2 > 0 && x1 < 0) *lon = -M_PI - *lon;
+}
+
 void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin) {
   auto pthermo = Thermodynamics::GetInstance();
 
@@ -82,7 +91,8 @@ void Forcing(MeshBlock *pmb, Real const time, Real const dt,
   int ie = pmb->ie, je = pmb->je, ke = pmb->ke;
   auto pthermo = Thermodynamics::GetInstance();
 
-  for (int k = ks; k <= ke; ++k)
+  // Cooling and Heating
+  /*for (int k = ks; k <= ke; ++k)
     for (int j = js; j <= je; ++j)
       for (int i = is; i <= ie; ++i) {
         auto &&air = AirParcelHelper::gather_from_primitive(pmb, k, j, i);
@@ -98,6 +108,112 @@ void Forcing(MeshBlock *pmb, Real const time, Real const dt,
         //   u(IEN,k,j,i) += w(IDN,k,j,i)*cv*(Tmin - temp)/sponge_tau*dt;
         // }
       }
+  */
+  for (int k = ks; k <= ke; ++k)
+    for (int j = js; j <= je; ++j) {
+      Real dz = pmb->pcoord->dx1f(pmb->is);
+      du(IEN, k, j, is) +=
+          hrate * dt / dz * (1. + 1.E-4 * sin(2. * M_PI * rand() / RAND_MAX));
+      ;
+      // Energy taken away from the top
+      dz = pmb->pcoord->dx1f(pmb->ie);
+      for (int i = is; i <= ie; ++i) {
+        if (w(IPR, k, j, i) < prad) {  // relax to Tmin profile
+          auto &&air = AirParcelHelper::gather_from_primitive(pmb, k, j, i);
+          Real cv = pthermo->GetCvMass(air, 0);
+          Real Rd = pthermo->GetRd();
+          Real Tnow = w(IPR, k, j, i) / Rd / w(IDN, k, j, i);
+          // du(IEN, k, j, i) += (Tmin - Tnow) * cv * w(IDN, k, j, i) * dt /
+          // 1E5;
+        }
+      }
+      // du(IEN, k, j, ie) -= hrate * dt / dz * (1. + 1.E-4 * sin(2. * M_PI *
+      // rand() / RAND_MAX));;
+    }
+
+  // Coriolis force
+  Real Omega = 1.74E-4;
+  for (int k = pmb->ks; k <= pmb->ke; ++k) {
+    for (int j = pmb->js; j <= pmb->je; ++j) {
+      for (int i = pmb->is; i <= pmb->ie; ++i) {
+        Real omega1, omega2;
+        // Load Latitude
+        Real lat, lon;
+        FindLatlon(&lat, &lon, pmb->pcoord->x3v(i), pmb->pcoord->x2v(j));
+
+        omega1 = cos(lat) * Omega;
+        omega2 = sin(lat) * Omega;  // f
+        Real m1 = w(IDN, k, j, i) * w(IVX, k, j, i);
+        Real m2 = w(IDN, k, j, i) * w(IVY, k, j, i);
+        Real m3 = w(IDN, k, j, i) * w(IVZ, k, j, i);
+        du(IM1, k, j, i) -= -2. * dt * (omega1 * m2);
+        du(IM2, k, j, i) -= 2. * dt * (omega1 * m1 - omega2 * m3);
+        du(IM3, k, j, i) -= 2. * dt * (omega2 * m2);
+      }
+    }
+  }
+  // Sponge layer and viscosity
+  Real sponge_lat = 75.0;
+  Real sponge_tau = 1E4;
+  Real vis = 0.;
+  Real Rp = 7E7;
+  for (int k = pmb->ks; k <= pmb->ke; ++k) {
+    for (int j = pmb->js; j <= pmb->je; ++j) {
+      for (int i = pmb->is; i <= pmb->ie; ++i) {
+        Real dist = sqrt(pmb->pcoord->x3v(i) * pmb->pcoord->x3v(i) +
+                         pmb->pcoord->x2v(j) * pmb->pcoord->x2v(j));
+        Real lat_now = 90. - dist / Rp / M_PI * 180.;
+        Real s = lat_now / sponge_lat;
+
+        if (s < 1) {
+          // Softer sponge layer, linear increasing from 0
+          du(IM1, k, j, i) -= dt * w(IDN, k, j, i) * w(IVX, k, j, i) /
+                              sponge_tau * (sponge_lat - lat_now) / 5;
+          du(IM2, k, j, i) -= dt * w(IDN, k, j, i) * w(IVY, k, j, i) /
+                              sponge_tau * (sponge_lat - lat_now) / 5;
+          du(IM3, k, j, i) -= dt * w(IDN, k, j, i) * w(IVZ, k, j, i) /
+                              sponge_tau * (sponge_lat - lat_now) / 5;
+        } else {  // viscosity
+          Real dz = pmb->pcoord->dx1f(i);
+          Real dx = pmb->pcoord->dx2f(j);
+          Real dy = pmb->pcoord->dx3f(k);
+          Real dir = IVX;
+          Real lap_1 = (w(dir, k, j, i + 1) - 2 * w(dir, k, j, i) +
+                        w(dir, k, j, i - 1)) /
+                           (dz * dz) +
+                       (w(dir, k, j + 1, i) - 2 * w(dir, k, j, i) +
+                        w(dir, k, j - 1, i)) /
+                           (dx * dx) +
+                       (w(dir, k + 1, j, i) - 2 * w(dir, k, j, i) +
+                        w(dir, k - 1, j, i)) /
+                           (dy * dy);
+          dir = IVY;
+          Real lap_2 = (w(dir, k, j, i + 1) - 2 * w(dir, k, j, i) +
+                        w(dir, k, j, i - 1)) /
+                           (dz * dz) +
+                       (w(dir, k, j + 1, i) - 2 * w(dir, k, j, i) +
+                        w(dir, k, j - 1, i)) /
+                           (dx * dx) +
+                       (w(dir, k + 1, j, i) - 2 * w(dir, k, j, i) +
+                        w(dir, k - 1, j, i)) /
+                           (dy * dy);
+          dir = IVZ;
+          Real lap_3 = (w(dir, k, j, i + 1) - 2 * w(dir, k, j, i) +
+                        w(dir, k, j, i - 1)) /
+                           (dz * dz) +
+                       (w(dir, k, j + 1, i) - 2 * w(dir, k, j, i) +
+                        w(dir, k, j - 1, i)) /
+                           (dx * dx) +
+                       (w(dir, k + 1, j, i) - 2 * w(dir, k, j, i) +
+                        w(dir, k - 1, j, i)) /
+                           (dy * dy);
+          du(IM1, k, j, i) += vis * dt * w(IDN, k, j, i) * lap_1;
+          du(IM2, k, j, i) += vis * dt * w(IDN, k, j, i) * lap_2;
+          du(IM3, k, j, i) += vis * dt * w(IDN, k, j, i) * lap_3;
+        }
+      }
+    }
+  }
 }
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
@@ -105,6 +221,9 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
 
   P0 = pin->GetReal("problem", "P0");
   T0 = pin->GetReal("problem", "T0");
+
+  // Real Omega = pin->GetReal("problem", "Omega");
+  // Real Rp = pin->GetReal("problem", "Rp");
 
   Tmin = pin->GetReal("problem", "Tmin");
   prad = pin->GetReal("problem", "prad");
