1c5e6f129331e0b3d3a11c21fa7f270a72e9fa1f
Cheng Li <chengcli@umich.edu>
2024-03-16 13:43:44 -0400

    wip

--- src/snap/thermodynamics/enroll_vapor_functions.cpp
+++ src/snap/thermodynamics/enroll_vapor_functions.cpp
@@ -8,29 +8,8 @@
 Real NullSatVaporPres1(AirParcel const& air, int i, int j) {
   Real g = 1.;
 
-<<<<<<< HEAD
 #pragma omp simd reduction(+ : g)
   for (int n = 0; n < NCLOUD; ++n) g += -air.c[n];
-=======
-#pragma omp simd reduction(- : g)
-  for (int n = 0; n < NCLOUD; ++n) g -= qfrac.c[n];
-
-  return qfrac.w[i] / g * qfrac.w[IPR];
-}
-
-void Thermodynamics::enrollVaporFunctions(ParameterInput* pin) {
-  if (NVAPOR == 0 && NCLOUD == 0) return;
-
-  if (strcmp(PLANET, "Earth") == 0) {
-    enrollVaporFunctionsEarth();
-  } else if (strcmp(PLANET, "Jupiter") == 0) {
-    enrollVaporFunctionsGiants();
-  } else {
-    throw RuntimeError("Thermodynamics",
-                       "Unknown planet: " + std::string(PLANET));
-  }
-}
->>>>>>> 027e882 (intel)
 
   return air.w[i] / g * air.w[IPR];
 }
