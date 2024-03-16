81ed0365c07fd9a5812cb2874739cbf0889f6fa4
mac/cli <chengcli@umich.edu>
2024-03-15 23:04:43 -0400

    wip

---
+++ src/exo3/cs_velocity_rotation.hpp
@@ -0,0 +1,9 @@
+#ifndef SRC_EXO3_VELOCITY_ROTATION_HPP_
+#define SRC_EXO3_VELOCITY_ROTATION_HPP_
+
+template <typename T>
+inline void cs_vel_p1_to_p2(T *v1, T *v2, T *v3) {
+  // fill in code here
+}
+
+#endif  // SRC_EXO3_VELOCITY_ROTATION_HPP_
