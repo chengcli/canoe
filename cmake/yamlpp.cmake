8b16d0399e6c0b77659fc08e341b6b977fbc8f34
Cheng Li <69489965+chengcli@users.noreply.github.com>
2024-03-16 20:41:23 -0400

    Make compile with icc (#131)
    - compile with -ipo requres  -fuse-ld=lld
    - SIMD currently does not work

--- cmake/yamlpp.cmake
+++ cmake/yamlpp.cmake
@@ -3,12 +3,20 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET TRUE)
 
 FetchContent_Declare(
-  yamlcpp
+  yaml-cpp
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-  URL https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-0.7.0.tar.gz
+  URL https://github.com/jbeder/yaml-cpp/archive/refs/tags/0.8.0.tar.gz
 )
 
-FetchContent_MakeAvailable(yamlcpp)
+FetchContent_GetProperties(yaml-cpp)
+
+if(NOT yaml-cpp_POPULATED)
+  message(STATUS "Fetching yaml-cpp...")
+  FetchContent_Populate(yaml-cpp)
+  add_subdirectory(${yaml-cpp_SOURCE_DIR} ${yaml-cpp_BINARY_DIR})
+endif()
+
+#FetchContent_MakeAvailable(yamlcpp)
 
 # Where yaml-cpp's .h files can be found.
-include_directories(${yamlcpp_SOURCE_DIR}/include)
+include_directories(${yaml-cpp_SOURCE_DIR}/include)
