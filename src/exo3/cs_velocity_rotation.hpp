a518bb79cea6f5d125814ad812a9a89b8303209b
jeffyujianfu <157439044+jeffyujianfu@users.noreply.github.com>
2024-04-01 19:17:09 -0400

    Update on coordinate transform between cubed sphere panels (#138)
    summary
    -add vs_velocity_rotation.hpp
    Defined transformation of velocity from panel 1 into neighboring panels and back.
    -add test_vs_velocity_rotation.cpp
    Test cases for the transformation, ensure that all inverses are valid.
    ---------
    
    Co-authored-by: mac/cli <chengcli@umich.edu>

---
+++ src/exo3/cs_velocity_rotation.hpp
@@ -0,0 +1,65 @@
+#ifndef SRC_EXO3_VELOCITY_ROTATION_HPP_
+#define SRC_EXO3_VELOCITY_ROTATION_HPP_
+
+#include <athena/athena.hpp>  // Real
+
+//! Panel number assiangments
+//! (1) Top
+//! (2) Front
+//! (3) Left
+//!
+//!                                           z
+//!                ___________                |
+//!                |\        .\           x___| (1)
+//!                | \   1   . \              /
+//!                |  \_________\           y/
+//!                | 3 |     .  |
+//!                \. .|......  |
+//! z___ (3)        \  |    2 . |
+//!    /|            \ |       .|             y
+//!   / |             \|________|             |
+//!  y  x                                 (2) |___x
+//!                                          /
+//!                                        z/
+
+//! Panel number assiangments
+//! (4) Right
+//! (5) Bottom
+//! (6) Back
+//!                                        y  x
+//!                __________              | /
+//!                |\       .\             |/___z
+//!                | \      . \           (4)
+//!     y  z       |  \________\
+//!     | /        |  |  6  .  |
+//! x___|/         |..|......  |
+//!      (6)       \  |     . 4|       (5) ___ x
+//!                 \ |  5   . |          /|
+//!                  \|_______.|         / |
+//!                                     y  z
+
+namespace CubedSphereUtility {
+
+//! Transform cubed sphere velocity to local cartesian velocity
+void vel_zab_to_zxy(Real *v1, Real *v2, Real *v3, Real a, Real b);
+//! Transform local cartesian velocity to cubed sphere velocity
+void vel_zxy_to_zab(Real *v1, Real *v2, Real *v3, Real a, Real b);
+
+//! Transform cubed sphere velocity from panel 1 to panel 2
+//! \param a $x = \tan(\xi)$ coordinates
+//! \param b $y = \tan(\eta)$ coordinat
+void vel_zab_from_p1(Real *vz, Real *vx, Real *vy, Real a, Real b,
+                            int panel);
+void vel_zab_from_p2(Real *vz, Real *vx, Real *vy, Real a, Real b,
+                            int panel);
+void vel_zab_from_p3(Real *vz, Real *vx, Real *vy, Real a, Real b,
+                            int panel);
+void vel_zab_from_p4(Real *vz, Real *vx, Real *vy, Real a, Real b,
+                            int panel);
+void vel_zab_from_p5(Real *vz, Real *vx, Real *vy, Real a, Real b,
+                            int panel);
+void vel_zab_from_p6(Real *vz, Real *vx, Real *vy, Real a, Real b,
+                            int panel);
+}  // namespace CubedSphereUtility
+
+#endif  // SRC_EXO3_VELOCITY_ROTATION_HPP_
---
+++ tests/test_cs_velocity_rotation.cpp
@@ -0,0 +1,86 @@
+// C/C++
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+// external
+#include <gtest/gtest.h>
+
+#include <application/application.hpp>
+
+// athena
+#include <athena/athena_arrays.hpp>
+
+// exo3
+#include <exo3/cs_velocity_rotation.hpp>
+
+TEST(vel_zab_to_zxy, test_ab_to_xy_to_ab) {
+  Real result[3] = {1,2,3};
+  Real *vz = result;
+  Real *vx = result + 1;
+  Real *vy = result + 2;
+  Real expected_result[3] = {1,2,3};
+  CubedSphereUtility::vel_zxy_to_zab(vz, vx, vy, PI/3, PI/4);
+  std::cout << *vz << " " << *vx << " " << *vy;
+  CubedSphereUtility::vel_zab_to_zxy(vz, vx, vy, PI/3, PI/4);
+  for (int i = 0; i < 3; i++) {
+    EXPECT_NEAR(result[i], expected_result[i], 1e-5);
+  }
+}
+
+TEST(vel_zab_from_test, test_case_1_to_2_to_1) {
+  Real result[3] = {1,2,3};
+  Real *vz = result;
+  Real *vx = result + 1;
+  Real *vy = result + 2;
+  Real expected_result[3] = {1,2,3};
+  CubedSphereUtility::vel_zab_from_p1(vz, vx, vy, PI/5*2, PI/8*3, 2);
+  CubedSphereUtility::vel_zab_from_p2(vz, vx, vy, PI/5*2, PI/8*3, 1);
+  for (int i = 0; i < 3; i++) {
+    EXPECT_NEAR(result[i], expected_result[i], 1e-5);
+  }
+}
+
+TEST(vel_zab_from_test, test_case_1_to_3_to_1) {
+  Real result[3] = {1,2,3};
+  Real *vz = result;
+  Real *vx = result + 1;
+  Real *vy = result + 2;
+  Real expected_result[3] = {1,2,3};
+  CubedSphereUtility::vel_zab_from_p1(vz, vx, vy, PI/5*2, PI/8*3, 3);
+  CubedSphereUtility::vel_zab_from_p3(vz, vx, vy, PI/5*2, PI/8*3, 1);
+  for (int i = 0; i < 3; i++) {
+    EXPECT_NEAR(result[i], expected_result[i], 1e-5);
+  }
+}
+
+TEST(vel_zab_from_test, test_case_1_to_4_to_1) {
+  Real result[3] = {1,2,3};
+  Real *vz = result;
+  Real *vx = result + 1;
+  Real *vy = result + 2;
+  Real expected_result[3] = {1,2,3};
+  CubedSphereUtility::vel_zab_from_p1(vz, vx, vy, PI/5*2, PI/8*3, 4);
+  CubedSphereUtility::vel_zab_from_p4(vz, vx, vy, PI/5*2, PI/8*3, 1);
+  for (int i = 0; i < 3; i++) {
+    EXPECT_NEAR(result[i], expected_result[i], 1e-5);
+  }
+}
+
+TEST(vel_zab_from_test, test_case_1_to_6_to_1) {
+  Real result[3] = {1,2,3};
+  Real *vz = result;
+  Real *vx = result + 1;
+  Real *vy = result + 2;
+  Real expected_result[3] = {1,2,3};
+  CubedSphereUtility::vel_zab_from_p1(vz, vx, vy, PI/5*2, PI/8*3, 6);
+  CubedSphereUtility::vel_zab_from_p6(vz, vx, vy, PI/5*2, PI/8*3, 1);
+  for (int i = 0; i < 3; i++) {
+    EXPECT_NEAR(result[i], expected_result[i], 1e-5);
+  }
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
