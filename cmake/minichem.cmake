2dfdbfe9b7d92376355d6e9e8360d414ebd7d587
ziggy/cli <chengcli@umich.edu>
2024-04-01 20:10:31 -0400

    wip

--- cmake/minichem.cmake
+++ cmake/minichem.cmake
@@ -2,18 +2,15 @@ include(FetchContent)
 
 set(FETCHCONTENT_QUIET FALSE)
 
-macro(set_if_empty _variable)
-  if("${${_variable}}" STREQUAL "")
-    set(${_variable} ${ARGN})
-  endif()
-endmacro()
-
 option(MINICHEM "Build minichem" OFF)
 
 if(MINICHEM)
   FetchContent_Declare(
-    mini_chem
+    minichem
     GIT_REPOSITORY https://github.com/chengcli/mini_chem
     GIT_TAG main)
-  FetchContent_MakeAvailable(mini_chem)
+  FetchContent_MakeAvailable(minichem)
+
+  include_directories(${minichem_SOURCE_DIR})
 endif()
+
