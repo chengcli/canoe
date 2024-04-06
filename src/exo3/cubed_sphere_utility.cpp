f0c4047d4f2ccd08433a72b93c3d917be2362693
mac/cli <chengcli@umich.edu>
2024-04-06 13:02:26 -0400

    cubed sphere edits

--- src/exo3/cubed_sphere_utility.cpp
+++ src/exo3/cubed_sphere_utility.cpp
@@ -9,6 +9,13 @@
 #include <athena/bvals/bvals.hpp>
 #include <athena/coordinates/coordinates.hpp>
 
+// application
+#include <application/exceptions.hpp>
+
+// canoe
+#include <configure.hpp>
+#include <impl.hpp>
+
 // exo3
 #include "cubed_sphere.hpp"
 #include "cubed_sphere_utility.hpp"
@@ -614,6 +621,21 @@ void PackData(const AthenaArray<Real> &src, Real *buf, int sn, int en, int si,
   }
   PackDataR0(src, buf, sn, en, si, ei, sj, ej, sk, ek, offset);
   return;
+}
+
+void get_latlon_on_sphere(Real *lat, Real *lon, MeshBlock const *pmb, int k,
+                          int j, int i) {
+#ifdef CUBED_SPHERE
+  pmb->pimpl->pexo3->GetLatLon(lat, lon, k, j, pmb->ie);
+#elif COORDINATE_SYSTEM == "spherical_polar"  // FIXME: add another condition
+  *lat = M_PI / 2. - pmb->pcoord->x2v(j);
+  *lon = pmb->pcoord->x3v(k);
+#else                                         // cartesian
+  std::stringstream msg;
+  msg << "Both Planet and TimeDependent flags are set, but coordinate system "
+      << COORDINATE_SYSTEM << " is not supported.";
+  throw RuntimeError("RTSolverDisort::Prepare", msg.str());
+#endif                                        // CUBED_SPHERE
 }
 
 }  // namespace CubedSphereUtility
