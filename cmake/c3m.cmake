853214e7a3943c4bc791ee1a225fd9664c1491aa
mac/cli <chengcli@umich.edu>
2024-04-26 08:58:35 -0400

    change c3m to flask

---
+++ cmake/c3m.cmake
@@ -0,0 +1,17 @@
+# Fetch c3m and build
+include(FetchContent)
+set(FETCHCONTENT_QUIET FALSE)
+
+set_if_empty(ACCOUNT $ENV{GH_ACCOUNT})
+set_if_empty(TOKEN $ENV{GH_TOKEN})
+
+option(C3M "Build C3M" OFF)
+
+if(C3M)
+  FetchContent_Declare(
+    c3m
+    GIT_REPOSITORY https://github.com/GalacticBobster/C3M
+    GIT_TAG cli/refactor)
+
+  FetchContent_MakeAvailable(c3m)
+endif()
