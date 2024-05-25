c248b9ef2fd4e9a83a4189bca151f7048498356d
mac/cli <chengcli@umich.edu>
2024-05-25 03:24:59 -0400

    wip

--- cmake/c3m.cmake
+++ cmake/c3m.cmake
@@ -10,8 +10,8 @@ option(C3M "Build C3M" OFF)
 if(C3M)
   FetchContent_Declare(
     c3m
-    GIT_REPOSITORY https://github.com/GalacticBobster/C3M
-    GIT_TAG cli/refactor)
+    GIT_REPOSITORY https://github.com/chengcli/C3M
+    GIT_TAG main)
 
   FetchContent_MakeAvailable(c3m)
 endif()
