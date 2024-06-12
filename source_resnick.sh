3a4885b92bb212a56e92a592c5668b38a77509a0
cli/hpc <lumos.leee@gmail.com>
2024-06-12 06:28:01 -0700

    add resnick

---
+++ source_resnick.sh
@@ -0,0 +1,6 @@
+
+module load cmake/3.20.2-gcc-11.3.1-iebch5b
+module load gcc/10.5.0-gcc-13.2.0-sbhl4sv
+module load eigen/3.4.0-gcc-11.3.1-jjz3uqe
+module load netcdf-c/4.9.2-gcc-11.3.1-p7dipnl
+#module load openmpi/4.1.5-gcc-13.2.0-24q3ap2
