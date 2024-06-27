d401ca291a89dbe8dfaa1e9dd07af26cfcb164a7
mac/cli <chengcli@umich.edu>
2024-06-27 10:29:10 -0400

    wip

--- tests/test_read_yaml.cpp
+++ tests/test_read_yaml.cpp
@@ -2,11 +2,9 @@
 #include <fstream>
 #include <vector>
 
-// cantera
-#include <cantera/base/yaml.h>
-
 // external
 #include <gtest/gtest.h>
+#include <yaml-cpp/yaml.h>
 
 class YamlReadTests : public ::testing::Test {
  protected:
