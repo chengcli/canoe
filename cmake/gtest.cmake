9873afb4a1bad1c5f831d39a4bef9ebf3046bf02
mac/cli <chengcli@umich.edu>
2024-07-20 03:11:17 -0400

    wip

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET TRUE)
 
 set(PACKAGE_NAME gtest)
-set(REPO_URL "https://github.com/chengcli/gtest")
+set(REPO_URL "https://github.com/google/googletest")
 set(REPO_TAG "v1.13.0")
 set(REPO_PATCH "None")
 set(INSTALL_GTEST OFF)
