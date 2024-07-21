2582030166367b5dfaa20526ec558c5117c24800
mac/cli <chengcli@umich.edu>
2024-07-20 21:20:45 -0400

    wip

---
+++ src/tracer/tracer.hpp
@@ -0,0 +1,26 @@
+#ifndef SRC_TRACER_TRACER_HPP_
+#define SRC_TRACER_TRACER_HPP_
+
+// C/C++
+#include <memory>
+
+// athenapp
+#include <athena/athena.hpp>
+
+class MeshBlock;
+class ParameterInput;
+
+class Tracer {
+ public:
+  AthenaArray<Real> w, u;
+
+  Tracer(MeshBlock *pmb, ParameterInput *pin);
+  ~Tracer();
+
+ protected:
+  MeshBlock *pmy_block_;
+};
+
+using TracerPtr = std::shared_ptr<Tracer>;
+
+#endif  //  SRC_TRACER_TRACER_HPP_
