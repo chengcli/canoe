5e43591b3b6e73361418862413e438dd954b042b
mac/cli <chengcli@umich.edu>
2024-07-25 15:12:50 -0400

    wip

--- src/exo3/exo3.hpp
+++ src/exo3/exo3.hpp
@@ -4,8 +4,8 @@
 
 // covariant velocity to contravariant velocity
 template <typename T>
-std::array<Real, 3> vec_raise(StrideIterator<T*> w, StrideIterator<T*> m) {
-  std::array<Real, 3> v{w[IVX], w[IVY], w[IVZ]};
+std::array<T, 3> vec_raise(StrideIterator<T*> w, StrideIterator<T*> m) {
+  std::array<T, 3> v{w[IVX], w[IVY], w[IVZ]};
   return v;
 }
 
@@ -13,8 +13,8 @@ template <typename T>
 void vec_raise_inplace(StrideIterator<T*> w, StrideIterator<T*> m) {}
 
 template <typename T>
-std::array<Real, 3> vec_lower(StrideIterator<T*> w, StrideIterator<T*> m) {
-  std::array<Real, 3> v{w[IVX], w[IVY], w[IVZ]};
+std::array<T, 3> vec_lower(StrideIterator<T*> w, StrideIterator<T*> m) {
+  std::array<T, 3> v{w[IVX], w[IVY], w[IVZ]};
   return v;
 }
 
