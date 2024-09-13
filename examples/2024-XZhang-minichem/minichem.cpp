4d56d0d13a7e730e10340dc9a2679ecf21a08268
Xi Zhang <xiz@ucsc.edu>
2024-09-13 12:49:34 -0700

    update minichem.cpp

--- examples/2024-XZhang-minichem/minichem.cpp
+++ examples/2024-XZhang-minichem/minichem.cpp
@@ -1,3 +1,14 @@
+/* -------------------------------------------------------------------------------------
+ * SNAP Example Program
+ *
+ * Contributer:
+ * Cheng Li, University of Michigan
+ *
+ * Year: 2023
+ * Contact: chengcli@umich.edu
+ * Reference: Test Jupiter CRM
+ * -------------------------------------------------------------------------------------
+ */
 // C++ headers
 #include <cmath>
 #include <iostream>
@@ -6,232 +17,246 @@
 #include <stdexcept>
 
 // athena
+#include <athena/athena.hpp>
+#include <athena/athena_arrays.hpp>
+#include <athena/bvals/bvals.hpp>
+#include <athena/coordinates/coordinates.hpp>
 #include <athena/eos/eos.hpp>
 #include <athena/field/field.hpp>
 #include <athena/hydro/hydro.hpp>
 #include <athena/mesh/mesh.hpp>
 #include <athena/parameter_input.hpp>
-#include <athena/stride_iterator.hpp>
 #include <athena/scalars/scalars.hpp>
 
+// application
+#include <application/application.hpp>
+#include <application/exceptions.hpp>
+
 // canoe
-#include <air_parcel.hpp>
-#include <configure.hpp>
 #include <impl.hpp>
-#include <athena/coordinates/coordinates.hpp>
+#include <index_map.hpp>
 
 // climath
 #include <climath/core.h>
-
-// exo3
-#include <exo3/cubed_sphere.hpp>
-#include <exo3/cubed_sphere_utility.hpp>
+#include <climath/interpolation.h>
 
 // snap
-#include <snap/thermodynamics/thermodynamics.hpp>
+#include <snap/stride_iterator.hpp>
+#include <snap/thermodynamics/atm_thermodynamics.hpp>
 
-// astro
-#include <astro/celestrial_body.hpp>
-
-// harp
-#include <harp/radiation.hpp>
+// special includes
+#include <special/giants_enroll_vapor_functions_v1.hpp>
 
 // minichem
 #include <minichem/mini_chem.hpp>
 
-Real Ps, Ts, Omega, grav, sponge_tau, bflux, gammad, Tmin;
-int sponge_layer;
-// species ['OH','H2','H2O','H','CO','CO2','O','CH4','C2H2','NH3','N2','HCN']
-std::vector<double> vmass = {17.01, 2.02, 18.02, 1.01, 28.01, 44.01, 16., 16.05, 26.04, 17.04, 28.02, 27.03};
-Real mmass = 2.238; //mean molecular mass in amu
+Real grav, P0, T0, Tmin, prad, hrate, xH2O;
+std::string met;
+int iH2O;
+bool use_mini;
 
+//'OH','H2','H2O','H','CO','CO2','O','CH4','C2H2','NH3','N2','HCN', 'He'
+std::vector<double> vmass = {17.01, 2.02,  18.02, 1.01,  28.01, 44.01, 16.,
+                             16.05, 26.04, 17.04, 28.02, 27.03, 4.};
 MiniChem *mc;
-/*
-void vmr_from_prim_scalar(std::vector<Real> &vmr, 
-                          AthenaArray<Real> const& prim_scalar, 
-                          AthenaArray<Real> const& w,
-                          int k, int j, int i)
-{
-  if (NCHEMISTRY > 0) {
-     for (int n=0; n<NCHEMISTRY; ++n) {
-        vmr[n] = prim_scalar(NCLOUD + n, k, j, i)/vmass[n]*mmass; //change mmr to vmr
-      }
-    }
-}
 
-void vmr_to_cons_scalar(AthenaArray<Real> &cons_scalar, 
-                        std::vector<Real> const& vmr, 
-                        AthenaArray<Real> const& w,
-                        int k, int j, int i)
-{
-  if (NCHEMISTRY > 0) {
-     for (int n=0; n<NCHEMISTRY; ++n) {
-        cons_scalar(NCLOUD + n, k, j, i) = w(IDN, k, j, i)*vmr[n]*vmass[n]/mmass; //change vmr to den
-      }
-    }
-}
-*/
 void MeshBlock::InitUserMeshBlockData(ParameterInput *pin) {
-  AllocateUserOutputVariables(7);
-  SetUserOutputVariableName(0, "temp");
-  SetUserOutputVariableName(1, "theta");
-  SetUserOutputVariableName(2, "lat");
-  SetUserOutputVariableName(3, "lon");
-  SetUserOutputVariableName(4, "vlat");
-  SetUserOutputVariableName(5, "vlon");
-  SetUserOutputVariableName(6, "zenith");
+  if (NVAPOR > 0) {
+    AllocateUserOutputVariables(5 + NVAPOR);
+    SetUserOutputVariableName(0, "temp");
+    SetUserOutputVariableName(1, "theta");
+    SetUserOutputVariableName(2, "pres");
+    SetUserOutputVariableName(3, "thetav");
+    SetUserOutputVariableName(4, "mse");
+    for (int n = 1; n <= NVAPOR; ++n) {
+      std::string name = "rh" + std::to_string(n);
+      SetUserOutputVariableName(4 + n, name.c_str());
+    }
+  } else {
+    AllocateUserOutputVariables(3);
+    SetUserOutputVariableName(0, "temp");
+    SetUserOutputVariableName(1, "theta");
+    SetUserOutputVariableName(2, "pres");
+  }
 }
 
 void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin) {
-  auto pexo3 = pimpl->pexo3;
   auto pthermo = Thermodynamics::GetInstance();
 
-  Real lat, lon;
-  Real U, V;
-  Direction ray = pimpl->prad->GetRayInput(0);
-  Real zenith;
-
   for (int k = ks; k <= ke; ++k)
-    for (int j = js; j <= je; ++j) {
+    for (int j = js; j <= je; ++j)
       for (int i = is; i <= ie; ++i) {
         user_out_var(0, k, j, i) = pthermo->GetTemp(this, k, j, i);
-        user_out_var(1, k, j, i) = pthermo->PotentialTemp(this, Ps, k, j, i);
+        user_out_var(1, k, j, i) = pthermo->PotentialTemp(this, P0, k, j, i);
+        user_out_var(2, k, j, i) = phydro->w(IPR, k, j, i);
 
-        pexo3->GetLatLon(&lat, &lon, k, j, i);
-        pexo3->GetUV(&U, &V, phydro->w(IVY, k, j, i), phydro->w(IVZ, k, j, i),
-                     k, j, i);
-        user_out_var(2, k, j, i) = lat;
-        user_out_var(3, k, j, i) = lon;
-        user_out_var(4, k, j, i) = U;
-        user_out_var(5, k, j, i) = V;
-
-        ray = pimpl->planet->ParentZenithAngle(pmy_mesh->time, M_PI / 2. - lat,
-                                               lon);
-        zenith = std::acos(ray.mu) / M_PI * 180.0;
-        user_out_var(6, k, j, i) = zenith;
+        if (NVAPOR > 0) {
+          // theta_v
+          user_out_var(3, k, j, i) =
+              user_out_var(1, k, j, i) * pthermo->RovRd(this, k, j, i);
+          // mse
+          user_out_var(4, k, j, i) =
+              pthermo->MoistStaticEnergy(this, grav * pcoord->x1v(i), k, j, i);
+          // relative humidity
+          for (int n = 1; n <= NVAPOR; ++n)
+            user_out_var(4 + n, k, j, i) =
+                pthermo->RelativeHumidity(this, n, k, j, i);
+        }
       }
-    }
 }
 
 void Forcing(MeshBlock *pmb, Real const time, Real const dt,
              AthenaArray<Real> const &w, const AthenaArray<Real> &prim_scalar,
              AthenaArray<Real> const &bcc, AthenaArray<Real> &du,
              AthenaArray<Real> &cons_scalar) {
-  auto pexo3 = pmb->pimpl->pexo3;
+  int is = pmb->is, js = pmb->js, ks = pmb->ks;
+  int ie = pmb->ie, je = pmb->je, ke = pmb->ke;
   auto pthermo = Thermodynamics::GetInstance();
-  auto prad = pmb->pimpl->prad;
   auto phydro = pmb->phydro;
+  std::vector<double> vmr(NCHEMISTRY);
+  std::vector<double> vmr_sp(NCHEMISTRY - 1);
+  std::vector<double> mmr(NCHEMISTRY);
+  Real sumVMR;
 
-  std::vector<Real> vmr(NCHEMISTRY);
+  for (int k = ks; k <= ke; ++k)
+    for (int j = js; j <= je; ++j)
+      for (int i = is; i <= ie; ++i) {
+        auto &&air = AirParcelHelper::gather_from_primitive(pmb, k, j, i);
 
-  for (int k = pmb->ks; k <= pmb->ke; ++k)
-    for (int j = pmb->js; j <= pmb->je; ++j) 
-      for (int i = pmb->is; i <= pmb->ie; ++i) {
-        Real lat, lon;
-        pexo3->GetLatLon(&lat, &lon, k, j, i);
+        air.ToMoleFraction();
+        Real cv =
+            get_cv_mass(air, 0, pthermo->GetRd(), pthermo->GetCvRatioMass());
 
-        // coriolis force
-        Real f = 2. * Omega * sin(lat);
-        Real f2 = 2. * Omega * cos(lat);
-        Real U, V;
-
-        pexo3->GetUV(&U, &V, w(IVY, k, j, i), w(IVZ, k, j, i), k, j, i);
-
-        Real m1 = w(IDN, k, j, i) * w(IVX, k, j, i);
-        Real m2 = w(IDN, k, j, i) * U;
-        Real m3 = w(IDN, k, j, i) * V;
-
-        Real ll_acc_U = f * m3;
-        Real ll_acc_V = -f * m2;
-        Real acc2, acc3;
-        pexo3->GetVyVz(&acc2, &acc3, ll_acc_U, ll_acc_V, k, j, i);
-        pexo3->ContravariantVectorToCovariant(j, k, acc2, acc3, &acc2, &acc3);
-        du(IM2, k, j, i) += dt * acc2;
-        du(IM3, k, j, i) += dt * acc3;
-
-        // minichem
-        Real temp = pthermo->GetTemp(w.at(k, j , i));
-        Real pres = w(IPR, k, j, i);
-
-        //change mmr to vmr
-        for (int n=0; n<NCHEMISTRY; ++n) vmr[n] = prim_scalar(NCLOUD + n, k, j, i)/vmass[n]*mmass; 
-//       vmr_from_prim_scalar(vmr, prim_scalar, w, k, j, i);
-        // call minichem        
-        mc->Run(temp, pres, dt, vmr.data(), "NCHO");
-
-       //normalize scale VMR to 1 and change to density
-        Real sumVMR = std::accumulate(vmr.begin(), vmr.end(), static_cast<Real>(0));
-        for (int n=0; n<NCHEMISTRY; ++n) {
-          cons_scalar(NCLOUD + n, k, j, i) = phydro->w(IDN, k, j, i)*vmr[n]/sumVMR*vmass[n]/mmass; 
+        if (w(IPR, k, j, i) < prad) {
+          du(IEN, k, j, i) += dt * hrate * w(IDN, k, j, i) * cv *
+                              (1. + 1.E-4 * sin(2. * M_PI * rand() / RAND_MAX));
         }
-//        vmr_to_cons_scalar(cons_scalar, vmr, w, k, j, i);
-
-/*        
-        Real rho = w(IDN, k, j, i);
-        if (i > pmb->is+10) {
-          for (int n = 0; n < NCHEMISTRY; ++n) {
-             cons_scalar(NCLOUD + n, k, j, i) -= rho * prim_scalar(NCLOUD + n, k, j, i) * n * dt / 1.E20;
-          }
-        } else {
-          for (int n = 0; n < NCHEMISTRY; ++n) {
-            cons_scalar(NCLOUD + n, k, j, i) = rho*1.E-6;
-          }
-        }
-*/
-
       }
-}
 
-Real TimeStep(MeshBlock *pmb) {
-  auto prad = pmb->pimpl->prad;
-  Real time = pmb->pmy_mesh->time;
+  if (use_mini) {
+    for (int k = ks; k <= ke; ++k)
+      for (int j = js; j <= je; ++j)
+        for (int i = is; i <= ie; ++i) {
+          // minichem
+          Real temp = pthermo->GetTemp(w.at(k, j, i));
+          Real pres = w(IPR, k, j, i);
 
-  for (int k = pmb->ks; k <= pmb->ke; ++k)
-    for (int j = pmb->js; j <= pmb->je; ++j) {
-      prad->CalTimeStep(pmb, k, j, pmb->is, pmb->ie);
-    }
+          // change mmr to vmr and normalize
+          for (int n = 0; n < NCHEMISTRY; ++n)
+            vmr[n] = prim_scalar(NCLOUD + n, k, j, i) / vmass[n];
+          sumVMR =
+              std::accumulate(vmr.begin(), vmr.end(), static_cast<Real>(0));
+          for (auto &value : vmr) value /= sumVMR;
 
-  return prad->GetTimeStep() * (1. + time / prad->GetRelaxTime());
+          // call minichem for sp species
+          for (int n = 0; n < NCHEMISTRY - 1; ++n) vmr_sp[n] = vmr[n];
+          if (temp > 200.) mc->Run(temp, pres, dt, vmr_sp.data(), "NCHO");
+          for (int n = 0; n < NCHEMISTRY - 1; ++n) vmr[n] = vmr_sp[n];
+
+          // change vmr to mmr and normalize
+          for (int n = 0; n < NCHEMISTRY; ++n) mmr[n] = vmr[n] * vmass[n];
+          sumVMR =
+              std::accumulate(mmr.begin(), mmr.end(), static_cast<Real>(0));
+          for (auto &value : mmr) value /= sumVMR;
+
+          for (int n = 0; n < NCHEMISTRY; ++n) {
+            cons_scalar(NCLOUD + n, k, j, i) +=
+                phydro->w(IDN, k, j, i) *
+                (mmr[n] - prim_scalar(NCLOUD + n, k, j, i));
+          }
+        }
+  }
 }
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
-
-  // forcing parameters
-  Omega = pin->GetReal("problem", "Omega");
   grav = -pin->GetReal("hydro", "grav_acc1");
-  Ts = pin->GetReal("problem", "Ts");
-  Ps = pin->GetReal("problem", "Ps");
-  gammad = pin->GetReal("hydro", "gamma");
+  P0 = pin->GetReal("problem", "P0");
+  T0 = pin->GetReal("problem", "T0");
   Tmin = pin->GetReal("problem", "Tmin");
+  prad = pin->GetReal("problem", "prad");
+  hrate = pin->GetReal("problem", "hrate") / 86400.;
+  use_mini = pin->GetOrAddBoolean("problem", "use_mini", true);
+  met = pin->GetOrAddString("problem", "metallicity", "1x");
 
-  // forcing function
+  if (NVAPOR > 0) {
+    // index
+    auto pindex = IndexMap::GetInstance();
+    iH2O = pindex->GetVaporId("H2O");
+  }
   EnrollUserExplicitSourceFunction(Forcing);
-  // EnrollUserTimeStepFunction(TimeStep);
-  
-  // minichem
-  mc = new MiniChem();
-  mc->SetDataFile("chem_data/mini_chem_data_NCHO.txt");
-  mc->SetSpeciesFile("chem_data/mini_chem_sp_NCHO.txt");
-  mc->SetNetworkDir("chem_data/1x/");
-  mc->SetMetallicityStr("1x");
-  mc->Initialize();
 
+  if (use_mini) {
+    // minichem
+    mc = new MiniChem();
+    mc->SetDataFile("chem_data/mini_chem_data_NCHO.txt");
+    mc->SetSpeciesFile("chem_data/mini_chem_sp_NCHO.txt");
+    mc->SetNetworkDir("chem_data/" + met + "/");
+    mc->SetMetallicityStr(met);
+    mc->Initialize();
+  }
 }
 
-//! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
-
   srand(Globals::my_rank + time(0));
-  auto pexo3 = pimpl->pexo3;
+
+  Application::Logger app("main");
+  app->Log("ProblemGenerator: jupiter_crm");
+
   auto pthermo = Thermodynamics::GetInstance();
 
+  // mesh limits
+  Real x1min = pmy_mesh->mesh_size.x1min;
+  Real x1max = pmy_mesh->mesh_size.x1max;
+
+  // request temperature and pressure
+  app->Log("request T", T0);
+  app->Log("request P", P0);
+
+  // thermodynamic constants
+  Real gamma = pin->GetReal("hydro", "gamma");
+  Real Rd = pthermo->GetRd();
+  Real cp = gamma / (gamma - 1.) * Rd;
+
+  // set up an adiabatic atmosphere
+  int max_iter = 400, iter = 0;
+  Real Ttol = pin->GetOrAddReal("problem", "init_Ttol", 0.01);
+
   AirParcel air(AirParcel::Type::MoleFrac);
-  srand(Globals::my_rank + time(0));
+
+  // estimate surface temperature and pressure
+  Real Ts = T0 - grav / cp * x1min;
+  Real Ps = P0 * pow(Ts / T0, cp / Rd);
+  if (NVAPOR > 0) xH2O = pin->GetReal("problem", "qH2O.ppmv") / 1.E6;
+
+  while (iter++ < max_iter) {
+    // read in vapors
+    if (NVAPOR > 0) air.w[iH2O] = xH2O;
+    air.w[IPR] = Ps;
+    air.w[IDN] = Ts;
+
+    // stop at just above P0
+    for (int i = is; i <= ie; ++i) {
+      pthermo->Extrapolate(&air, pcoord->dx1f(i), "pseudo", grav);
+      if (air.w[IPR] < P0) break;
+    }
+
+    // make up for the difference
+    Ts += T0 - air.w[IDN];
+    if (std::abs(T0 - air.w[IDN]) < Ttol) break;
+
+    app->Log("Iteration #", iter);
+    app->Log("T", air.w[IDN]);
+  }
+
+  if (iter > max_iter) {
+    throw RuntimeError("ProblemGenerator", "maximum iteration reached");
+  }
 
   // construct atmosphere from bottom up
   for (int k = ks; k <= ke; ++k)
     for (int j = js; j <= je; ++j) {
       air.SetZero();
+      if (NVAPOR > 0) air.w[iH2O] = xH2O;
       air.w[IPR] = Ps;
       air.w[IDN] = Ts;
 
@@ -244,7 +269,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
         if (air.w[IDN] < Tmin) break;
         AirParcelHelper::distribute_to_primitive(this, k, j, i, air);
         AirParcelHelper::distribute_to_conserved(this, k, j, i, air);
-        pthermo->Extrapolate(&air, pcoord->dx1f(i), "pseudo", grav);
+        pthermo->Extrapolate(&air, pcoord->dx1f(i), "pseudo", grav, 1.e-5);
       }
 
       // Replace adiabatic atmosphere with isothermal atmosphere if temperature
@@ -257,32 +282,45 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
       }
     }
 
-  // initialize conserved variables
-  Real Rd = pthermo->GetRd();
-  int n_sp = 13;
-  // interpolate from ce table
-  std::vector<double> vmr_ic(n_sp);
-  std::string ic_file = "chem_data/IC/mini_chem_IC_FastChem_1x.txt";
+  if (use_mini) {
+    // minichem initialize conserved variables
+    // interpolate from ce table
+    std::vector<double> vmr_ic(NCHEMISTRY);
+    std::vector<double> mmr(NCHEMISTRY);
+    std::string ic_file = "chem_data/IC/mini_chem_IC_FastChem_" + met + ".txt";
 
-  for (int k=ks; k<=ke; k++) 
-    for (int j=js; j<=je; j++) 
-      for (int i=is; i<=ie; i++) {
-         double mu;
-         double T_in = pthermo->GetTemp(this, k, j, i);
-         double P_in = phydro->w(IPR, k, j, i);
-//         std::cout<<"interp  "<<P_in<<" "<<T_in<<std::endl;
-         interp_ce_table(n_sp, T_in, P_in, vmr_ic.data(), &mu, ic_file);
+    for (int k = ks; k <= ke; k++)
+      for (int j = js; j <= je; j++)
+        for (int i = is; i <= ie; i++) {
+          double mu;
+          double T_in = pthermo->GetTemp(this, k, j, i);
+          double P_in = phydro->w(IPR, k, j, i);
+          interp_ce_table(NCHEMISTRY, T_in, P_in, vmr_ic.data(), &mu, ic_file);
 
-        //normalize scale VMR to 1
-        Real sumVMR = std::accumulate(vmr_ic.begin(), vmr_ic.end(), static_cast<Real>(0));
-        for (auto& value : vmr_ic) {value /= sumVMR;}
-   
-         if (NCHEMISTRY > 0) {
-           for (int n=0; n<NCHEMISTRY; ++n) {
-//           std::cout<<"mini  "<<rho<<" "<<n<<" "<<vmr_ic[n]<<" "<<vmass[n]<<" "<<mu<<std::endl;
-            pscalars->s(NCLOUD+n,k,j,i) = phydro->w(IDN, k, j, i)*vmr_ic[n]*vmass[n]/mmass;
-           }
-         }
-      }
+          // change vmr to mmr and normalize
+          for (int n = 0; n < NCHEMISTRY; ++n) mmr[n] = vmr_ic[n] * vmass[n];
+          Real sumVMR =
+              std::accumulate(mmr.begin(), mmr.end(), static_cast<Real>(0));
+          for (auto &value : mmr) value /= sumVMR;
 
+          for (int n = 0; n < NCHEMISTRY; ++n) {
+            pscalars->s(NCLOUD + n, k, j, i) +=
+                phydro->w(IDN, k, j, i) * mmr[n];
+          }
+        }
+  } else {
+    Real xtra = pin->GetReal("problem", "tracer.ppb") / 1.E9;
+    Real ptra = pin->GetReal("problem", "tracer.pre");
+    for (int k = ks; k <= ke; k++)
+      for (int j = js; j <= je; j++)
+        for (int i = is; i <= ie; i++) {
+          double P_in = phydro->w(IPR, k, j, i);
+          if (P_in > ptra) {
+            for (int n = 0; n < NCHEMISTRY; ++n) {
+              pscalars->s(NCLOUD + n, k, j, i) +=
+                  phydro->w(IDN, k, j, i) * xtra;
+            }
+          }
+        }
+  }
 }
