c978fb97495251c6d4a82fb5fba8b686bf4a59b9
canopy/cli <chengcli@umich.edu>
2024-12-06 18:18:45 -0500

    wip

--- external/install_cantera.sh
+++ external/install_cantera.sh
@@ -4,11 +4,12 @@ CXX=g++
 CC=gcc
 cxx_flags="-std=c++17 -D_GLIBCXX_USE_CXX11_ABI=0"
 prefix="${HOME}/opt/"
+boost_inc_dir=`pwd`/cantera/ext/cliboost/
 python_package=y
 f90_interface=n
 system_eigen=n
 system_blas_lapack=n
-boost_inc_dir=`pwd`/cantera/ext/cliboost/
+hdf5_support=n
 
 cd cantera
 git submodule init ext/cliboost
@@ -18,6 +19,7 @@ scons build CXX=${CXX} CC=${CC} cxx_flags=${cxx_flags} prefix=${prefix} \
   python_package=${python_package} f90_interface=${f90_interface} \
   system_eigen=${system_eigen} \
   system_blas_lapack=${system_blas_lapack} \
+  hdf_support=${hdf_support} \
   boost_inc_dir="${boost_inc_dir}" -j8
 scons install
 cd ..
