acd9c4303a616c920692d3d24c37937dc9a9a6ed
mac/cli <chengcli@umich.edu>
2024-12-06 18:11:26 -0500

    wip

--- external/install_cantera.sh
+++ external/install_cantera.sh
@@ -2,8 +2,8 @@
 
 CXX=g++
 CC=gcc
-cxx_flags=-std=c++17
-prefix=${HOME}/opt/
+cxx_flags="-std=c++17 -D_GLIBCXX_USE_CXX11_ABI=0"
+prefix="${HOME}/opt/"
 python_package=y
 f90_interface=n
 system_eigen=n
@@ -18,6 +18,6 @@ scons build CXX=${CXX} CC=${CC} cxx_flags=${cxx_flags} prefix=${prefix} \
   python_package=${python_package} f90_interface=${f90_interface} \
   system_eigen=${system_eigen} \
   system_blas_lapack=${system_blas_lapack} \
-  boost_inc_dir=${boost_inc_dir} -j8
+  boost_inc_dir="${boost_inc_dir}" -j8
 scons install
 cd ..
