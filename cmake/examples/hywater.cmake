45d1844fc64ef8714797aa3e4b4f056a2908871f
mac/cli <chengcli@umich.edu>
2024-12-07 12:22:32 -0500

    add hywater

--- cmake/examples/hywater.cmake
+++ cmake/examples/hywater.cmake
@@ -1,21 +1,13 @@
 # configure file for hydrogen-water world
 
-macro(SET_IF_EMPTY _variable)
-  if("${${_variable}}" STREQUAL "")
-    set(${_variable} ${ARGN})
-  endif()
-endmacro()
-
 # athena variables
-set_if_empty(NUMBER_GHOST_CELLS 3)
+set(NUMBER_GHOST_CELLS 3)
 
 # canoe configure
 set(NVAPOR 1)
-set(NCLOUD 2)
-set(NPHASE_LEGACY 3)
-set(NETCDF OFF)
+set(NCLOUD 1)
+set(NPRECIP 1)
 set(PNETCDF ON)
 set(MPI ON)
-set(DISORT ON)
-set(PYTHON_BINDINGS ON)
+set(EQUATION_OF_STATE ideal_moist)
 set(RSOLVER lmars)
