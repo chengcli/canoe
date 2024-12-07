a08b33bd7d0256df16333ef950d0bd6adffe61c9
Cheng Li <chengcli@umich.edu>
2024-12-06 22:06:06 -0500

    remove python

--- external/install_cantera.sh
+++ external/install_cantera.sh
@@ -4,18 +4,21 @@ CXX=g++
 CC=gcc
 prefix=${HOME}/opt/
 boost_inc_dir=`pwd`/cantera/ext/cliboost/
-python_package=y
+python_package=n
 f90_interface=n
 system_eigen=n
 system_blas_lapack=n
 hdf_support=n
+python_sdist=n
 
 cd cantera
 git submodule init ext/cliboost
 git submodule update ext/cliboost
 
 scons build CXX=${CXX} CC=${CC} cxx_flags="-std=c++17 -D_GLIBCXX_USE_CXX11_ABI=0" \
-  python_package=${python_package} f90_interface=${f90_interface} \
+  python_package=${python_package} \
+  python_sdist=${python_sdist} \
+  f90_interface=${f90_interface} \
   system_eigen=${system_eigen} \
   system_blas_lapack=${system_blas_lapack} \
   hdf_support=${hdf_support} \
