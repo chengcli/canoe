f8b9d6dd9ab71fb2bfea93cba8f9901575fd03b8
mac/cli <chengcli@umich.edu>
2025-06-05 09:30:34 -0500

    wip

--- cmake/pyharp.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-set(FETCHCONTENT_QUIET TRUE)
-
-set(PACKAGE_NAME pyharp)
-set(REPO_URL "https://github.com/chengcli/pyharp")
-set(REPO_TAG "v1.6.3")
-
-add_package(${PACKAGE_NAME} ${REPO_URL} ${REPO_TAG} "" ON)
-include_directories(${pyharp_SOURCE_DIR})
