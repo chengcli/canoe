e7c18d3d8fb6ffce72e4be041459f76ade34b670
mac/cli <chengcli@umich.edu>
2025-09-05 15:55:05 -0400

    wip

--- examples/2024-XZhang-minichem/read_tensor.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-# load the first file to infer shapes
-module = torch.jit.load(pt_files[0][1])
-data = {name: param for name, param in module.named_parameters()}
-
-print(data.keys())
-print(data["hydro_u/0"].shape)
-print(data["scalar_s/1"].shape)
